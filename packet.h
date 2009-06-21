78162d4b15c9e6d56b62cd77dbf7143b9c455873
Marek Lindner <lindner_marek@yahoo.de>
2009-06-21 11:07:18 +0200

    [batman-adv] add baticmp version check

--- packet.h
+++ packet.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2006 B.A.T.M.A.N. contributors:
- * Marek Lindner
+ * Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
+ * Marek Lindner, Simon Wunderlich
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
  * License as published by the Free Software Foundation.
@@ -19,48 +19,51 @@
 
 
 
-#define ETH_P_BATMAN  0x4305    /* unofficial/not registered Ethertype */
+#define ETH_P_BATMAN  0x4305	/* unofficial/not registered Ethertype */
 
 #define BAT_PACKET    0x01
 #define BAT_ICMP      0x02
 #define BAT_UNICAST   0x03
 #define BAT_BCAST     0x04
+#define BAT_VIS       0x05
+
+/* this file is included by batctl which needs these defines */
+#define COMPAT_VERSION 7
+#define DIRECTLINK 0x40
+#define VIS_SERVER 0x20
 
 
-#define COMPAT_VERSION 6
-
+/* ICMP message types */
 #define ECHO_REPLY 0
 #define DESTINATION_UNREACHABLE 3
 #define ECHO_REQUEST 8
 #define TTL_EXCEEDED 11
-
+#define PARAMETER_PROBLEM 12
 
 
 struct batman_packet
 {
 	uint8_t  packet_type;
 	uint8_t  version;  /* batman version field */
-	uint8_t  flags;    /* 0x80: UNIDIRECTIONAL link, 0x40: DIRECTLINK flag, ... */
-	uint8_t  ttl;
-	uint8_t  gwflags;  /* flags related to gateway functions: gateway class */
+	uint8_t  flags;    /* 0x40: DIRECTLINK flag, 0x20 VIS_SERVER flag... */
 	uint8_t  tq;
 	uint16_t seqno;
 	uint8_t  orig[6];
 	uint8_t  old_orig[6];
+	uint8_t  ttl;
 	uint8_t  num_hna;
-	uint8_t  pad;
 } __attribute__((packed));
 
 struct icmp_packet
 {
 	uint8_t  packet_type;
 	uint8_t  version;  /* batman version field */
-	uint8_t  msg_type;   /* 0 = ECHO REPLY, 3 = DESTINATION_UNREACHABLE, 8 = ECHO_REQUEST, 11 = TTL exceeded */
+	uint8_t  msg_type;   /* see ICMP message types above */
+	uint8_t  ttl;
 	uint8_t  dst[6];
 	uint8_t  orig[6];
-	uint8_t  ttl;
-	uint8_t  uid;
 	uint16_t seqno;
+	uint8_t  uid;
 } __attribute__((packed));
 
 struct unicast_packet
@@ -79,3 +82,18 @@ struct bcast_packet
 	uint16_t seqno;
 } __attribute__((packed));
 
+struct vis_packet
+{
+	uint8_t  packet_type;
+	uint8_t  version;  			/* batman version field */
+	uint8_t  vis_type;			/* which type of vis-participant sent this? */
+	uint8_t  seqno;				/* sequence number */
+	uint8_t  entries;			/* number of entries behind this struct */
+	uint8_t  ttl;				/* TTL */
+	uint8_t  vis_orig[6];		/* originator that informs about its neighbours */
+	uint8_t  target_orig[6];	/* who should receive this packet */
+	uint8_t  sender_orig[6];	/* who sent or rebroadcasted this packet */
+} __attribute__((packed));
+
+#define VIS_TYPE_SERVER_SYNC		0
+#define VIS_TYPE_CLIENT_UPDATE		1

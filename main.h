a3234814634645e9a8c893c6f98ec826a8fd0747
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2010-06-18 20:09:51 +0200

    batctl-2010.0.0: adjust version number

--- main.h
+++ main.h
@@ -21,6 +21,6 @@
 
 
 
-#define SOURCE_VERSION "0.2.2-beta"  /*put exactly one distinct word inside the string like "0.3-pre-alpha" or "0.3-rc1" or "0.3" */
+#define SOURCE_VERSION "2010.0.0"
 
 #define BAT_DEVICE "/dev/batman-adv"

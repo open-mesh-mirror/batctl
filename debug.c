c1f7b3f7f7a7aa3f100d9a7a71c77a6058f60c48
Marek Lindner <lindner_marek@yahoo.de>
2010-10-09 11:39:26 +0200

    batctl: display soft-interface neighbor table
    Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- debug.c
+++ debug.c
@@ -61,6 +61,15 @@ void trans_global_usage(void)
 	printf(" \t -w [interval] watch mode - refresh the global translation table continuously\n");
 }
 
+void softif_neigh_usage(void)
+{
+	printf("Usage: batctl [options] softif_neigh \n");
+	printf("options:\n");
+	printf(" \t -h print this help\n");
+	printf(" \t -n don't replace mac addresses with bat-host names\n");
+	printf(" \t -w [interval] watch mode - refresh the soft-interface neighbor table continuously\n");
+}
+
 void gateways_usage(void)
 {
 	printf("Usage: batctl [options] gateways \n");

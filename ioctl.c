open-mesh-mirror/batctl

commit 894b25c2be5f62f8eca2d71a37c5e2aa8a418789

batctl: Make root privileges check function specific
It is a long standing TODO to move the root check to each batctl
sub-application. This will allow later to make the checks specific to the
requirements for each function instead of disallowing the use of batctl for
non-root users completely.

Signed-off-by: Sven Eckelmann <sven@narfation.org>
Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

ioctl.c
@@ -33,6 +33,7 @@
 #include <linux/ethtool.h>
 #include <stdint.h>
 
+#include "functions.h"
 #include "ioctl.h"
 
 /* code borrowed from ethtool */
@@ -103,6 +104,8 @@ int ioctl_statistics_get(char *mesh_iface)
 {
 	struct ifreq ifr;
 	int fd = -1, ret = EXIT_FAILURE;
+
+	check_root_or_die("batctl statistics");
 
 	memset(&ifr, 0, sizeof(ifr));
 	strncpy(ifr.ifr_name, mesh_iface, sizeof(ifr.ifr_name));

debug.c
@@ -219,6 +219,8 @@ int handle_debug_table(char *mesh_iface, int debug_table, int argc, char **argv)
 		}
 	}
 
+	check_root_or_die("batctl");
+
 	if (read_opt & UNICAST_ONLY && read_opt & MULTICAST_ONLY) {
 		fprintf(stderr, "Error - '-u' and '-m' are exclusive options\n");
 		debug_table_usage(debug_table);
@@ -270,6 +272,8 @@ int print_vis_info(char *mesh_iface)
 	char *debugfs_mnt;
 	FILE *fp;
 
+	check_root_or_die("batctl vis_data");
+
 	debugfs_mnt = debugfs_mount(NULL);
 	if (!debugfs_mnt) {
 		fprintf(stderr, "Error - can't mount or find debugfs\n");
@@ -317,6 +321,8 @@ int log_print(char *mesh_iface, int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 	}
+
+	check_root_or_die("batctl log");
 
 	debugfs_mnt = debugfs_mount(NULL);
 	if (!debugfs_mnt) {

interface.c
@@ -324,6 +324,8 @@ int interface(char *mesh_iface, int argc, char **argv)
 		}
 	}
 
+	check_root_or_die("batctl interface");
+
 	rest_argc = argc - optind;
 	rest_argv = &argv[optind];
 

main.c
@@ -134,11 +134,6 @@ int main(int argc, char **argv)
 		exit(EXIT_SUCCESS);
 	}
 
-	/* TODO: remove this generic check here and move it into the individual functions */
-	/* check if user is root */
-	if (strncmp(argv[1], "bisect", strlen("bisect")) != 0)
-		check_root_or_die(argv[0]);
-
 	if ((strcmp(argv[1], "interface") == 0) || (strcmp(argv[1], "if") == 0)) {
 
 		ret = interface(mesh_iface, argc - 1, argv + 1);

ping.c
@@ -133,6 +133,8 @@ int ping(char *mesh_iface, int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	check_root_or_die("batctl ping");
+
 	dst_string = argv[found_args];
 	bat_hosts_init(0);
 	bat_host = bat_hosts_find_by_name(dst_string);

sys.c
@@ -152,6 +152,8 @@ int handle_loglevel(char *mesh_iface, int argc, char **argv)
 		}
 	}
 
+	check_root_or_die("batctl loglevel");
+
 	path_buff = malloc(PATH_BUFF_LEN);
 	snprintf(path_buff, PATH_BUFF_LEN, SYS_BATIF_PATH_FMT, mesh_iface);
 
@@ -251,6 +253,8 @@ int handle_sys_setting(char *mesh_iface, int setting, int argc, char **argv)
 		}
 	}
 
+	check_root_or_die("batctl");
+
 	/* prepare the classic path */
 	path_buff = malloc(PATH_BUFF_LEN);
 	snprintf(path_buff, PATH_BUFF_LEN, SYS_BATIF_PATH_FMT, mesh_iface);
@@ -323,6 +327,8 @@ int handle_gw_setting(char *mesh_iface, int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 	}
+
+	check_root_or_die("batctl gw_mode");
 
 	path_buff = malloc(PATH_BUFF_LEN);
 	snprintf(path_buff, PATH_BUFF_LEN, SYS_BATIF_PATH_FMT, mesh_iface);
@@ -448,6 +454,8 @@ int handle_ra_setting(int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 	}
+
+	check_root_or_die("batctl routing_algo");
 
 	if (argc == 2) {
 		res = write_file(SYS_SELECTED_RA_PATH, "", argv[1], NULL);

tcpdump.c
@@ -1255,6 +1255,8 @@ int tcpdump(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	check_root_or_die("batctl tcpdump");
+
 	bat_hosts_init(read_opt);
 
 	signal(SIGINT, sig_handler);

tp_meter.c
@@ -432,6 +432,8 @@ int tp_meter(char *mesh_iface, int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	check_root_or_die("batctl throughputmeter");
+
 	dst_string = argv[found_args];
 	bat_hosts_init(read_opt);
 	bat_host = bat_hosts_find_by_name(dst_string);

commit e6ae9965ccd774a4e914b8b8c9ba72df1187fbd5

batctl: Work around musl conflicts with linux/ethtool.h
linux/ethtool.h and musl's net/ethernet.h cannot be used at the same time.
Otherwise the compiles failes with

    In file included from include/net/ethernet.h:10:0,
                     from functions.h:25,
                     from ioctl.c:36:
    include/netinet/if_ether.h:101:8: error: redefinition of 'struct ethhdr'
     struct ethhdr {
            ^
    In file included from include/linux/ethtool.h:17:0,
                     from ioctl.c:33:
    include/linux/if_ether.h:140:8: note: originally defined here
     struct ethhdr {
            ^
    Makefile:123: recipe for target 'ioctl.o' failed

Fixes: 894b25c2be5f ("batctl: Make root privileges check function specific")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

ioctl.c
@@ -33,8 +33,9 @@
 #include <linux/ethtool.h>
 #include <stdint.h>
 
-#include "functions.h"
 #include "ioctl.h"
+
+void check_root_or_die(const char *cmd);
 
 /* code borrowed from ethtool */
 static int statistics_custom_get(int fd, struct ifreq *ifr)
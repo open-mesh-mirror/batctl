open-mesh-mirror/batctl

commit 23f39261428e020955d798589c12e4719eddabdb

batctl: Replace '-m meshif' option with selector prefix
The '-m meshif' option doesn't follow the prefix style which is used for
VLANs/hard interfaces. It is also not clear for a user which command
understands the '-m' option and which is actually operating on a
global/non-meshif specific scope. To clean this up, an additional command
prefix called meshif is implemented:

  $ batctl meshif bat0 ...

This can be omitted when it would be "meshif bat0". And when it is
specified, all non-meshif specific subcommands will be rejected.

The old option '-m meshif' option is still available to avoid breaking
existing scripts. It will will show an error message but continues to work
for now. Users of batctl should still transition to the new format.

Signed-off-by: Sven Eckelmann <sven@narfation.org>

main.c
@@ -35,6 +35,7 @@ static void print_usage(void)
 		{
 			.label = "commands:\n",
 			.types = BIT(SUBCOMMAND) |
+				 BIT(SUBCOMMAND_MIF) |
 				 BIT(SUBCOMMAND_VID) |
 				 BIT(SUBCOMMAND_HIF),
 		},
@@ -47,9 +48,13 @@ static void print_usage(void)
 		"",
 		NULL,
 	};
+	const char *meshif_prefixes[] = {
+		"meshif <netdev> ",
+		NULL,
+	};
 	const char *vlan_prefixes[] = {
 		"vlan <vdev> ",
-		"vid <vid> ",
+		"meshif <netdev> vid <vid> ",
 		NULL,
 	};
 	const char *hardif_prefixes[] = {
@@ -64,7 +69,6 @@ static void print_usage(void)
 
 	fprintf(stderr, "Usage: batctl [options] command|debug table [parameters]\n");
 	fprintf(stderr, "options:\n");
-	fprintf(stderr, " \t-m mesh interface (default 'bat0')\n");
 	fprintf(stderr, " \t-h print this help (or 'batctl <command|debug table> -h' for the parameter help)\n");
 	fprintf(stderr, " \t-v print version\n");
 
@@ -83,6 +87,10 @@ static void print_usage(void)
 				continue;
 
 			switch (cmd->type) {
+			case DEBUGTABLE:
+			case SUBCOMMAND_MIF:
+				prefixes = meshif_prefixes;
+				break;
 			case SUBCOMMAND_VID:
 				prefixes = vlan_prefixes;
 				break;
@@ -102,7 +110,7 @@ static void print_usage(void)
 					snprintf(buf, sizeof(buf), "%s%s|%s",
 						 *prefix, cmd->name, cmd->abbr);
 
-				fprintf(stderr, " \t%-35s%s\n", buf,
+				fprintf(stderr, " \t%-43s%s\n", buf,
 					cmd->usage);
 			}
 		}
@@ -155,8 +163,11 @@ static const struct command *find_command(struct state *state, const char *name)
 
 	switch (state->selector) {
 	case SP_NONE_OR_MESHIF:
-		types = BIT(SUBCOMMAND) |
-			BIT(DEBUGTABLE);
+		types = BIT(SUBCOMMAND);
+		/* fall through */
+	case SP_MESHIF:
+		types |= BIT(SUBCOMMAND_MIF) |
+			 BIT(DEBUGTABLE);
 		break;
 	case SP_VLAN:
 		types = BIT(SUBCOMMAND_VID);
@@ -179,7 +190,10 @@ static int detect_selector_prefix(int argc, char *argv[],
 		return -EINVAL;
 
 	/* only detect selector prefix which identifies meshif */
-	if (strcmp(argv[0], "vlan") == 0) {
+	if (strcmp(argv[0], "meshif") == 0) {
+		*selector = SP_MESHIF;
+		return 2;
+	} else if (strcmp(argv[0], "vlan") == 0) {
 		*selector = SP_VLAN;
 		return 2;
 	} else if (strcmp(argv[0], "hardif") == 0) {
@@ -204,6 +218,11 @@ static int parse_meshif_args(struct state *state, int argc, char *argv[])
 	dev_arg = argv[parsed_args - 1];
 
 	switch (selector) {
+	case SP_MESHIF:
+		snprintf(state->mesh_iface, sizeof(state->mesh_iface), "%s",
+			 dev_arg);
+		state->selector = SP_MESHIF;
+		return parsed_args;
 	case SP_VLAN:
 		ret = translate_vlan_iface(state, dev_arg);
 		if (ret < 0) {
@@ -252,6 +271,7 @@ static int parse_dev_args(struct state *state, int argc, char *argv[])
 
 	switch (state->selector) {
 	case SP_NONE_OR_MESHIF:
+	case SP_MESHIF:
 		/* continue below */
 		break;
 	default:
@@ -297,6 +317,7 @@ int main(int argc, char **argv)
 					"Error - multiple mesh interfaces specified\n");
 				goto err;
 			}
+			fprintf(stderr, "Warning - option -m was deprecated in will be removed in the future\n");
 
 			state.arg_iface = argv[2];
 			break;

aggregation.c
@@ -55,7 +55,7 @@ static struct settings_data batctl_settings_aggregation = {
 	.netlink_set = set_aggregated_ogms,
 };
 
-COMMAND_NAMED(SUBCOMMAND, aggregation, "ag", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, aggregation, "ag", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_aggregation,
 	      "[0|1]             \tdisplay or modify aggregation setting");

ap_isolation.c
@@ -77,7 +77,7 @@ static struct settings_data batctl_settings_ap_isolation = {
 	.netlink_set = set_ap_isolation,
 };
 
-COMMAND_NAMED(SUBCOMMAND, ap_isolation, "ap", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, ap_isolation, "ap", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_ap_isolation,
 	      "[0|1]             \tdisplay or modify ap_isolation setting");

bonding.c
@@ -54,7 +54,7 @@ static struct settings_data batctl_settings_bonding = {
 	.netlink_set = set_bonding,
 };
 
-COMMAND_NAMED(SUBCOMMAND, bonding, "b", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, bonding, "b", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_bonding,
 	      "[0|1]             \tdisplay or modify bonding setting");

bridge_loop_avoidance.c
@@ -55,7 +55,7 @@ static struct settings_data batctl_settings_bridge_loop_avoidance = {
 	.netlink_set = set_bridge_loop_avoidance,
 };
 
-COMMAND_NAMED(SUBCOMMAND, bridge_loop_avoidance, "bl", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, bridge_loop_avoidance, "bl", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_bridge_loop_avoidance,
 	      "[0|1]             \tdisplay or modify bridge_loop_avoidance setting");

distributed_arp_table.c
@@ -55,7 +55,7 @@ static struct settings_data batctl_settings_distributed_arp_table = {
 	.netlink_set = set_distributed_arp_table,
 };
 
-COMMAND_NAMED(SUBCOMMAND, distributed_arp_table, "dat", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, distributed_arp_table, "dat", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_distributed_arp_table,
 	      "[0|1]             \tdisplay or modify distributed_arp_table setting");

fragmentation.c
@@ -55,7 +55,7 @@ static struct settings_data batctl_settings_fragmentation = {
 	.netlink_set = set_fragmentation,
 };
 
-COMMAND_NAMED(SUBCOMMAND, fragmentation, "f", handle_sys_setting,
+COMMAND_NAMED(SUBCOMMAND_MIF, fragmentation, "f", handle_sys_setting,
 	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
 	      &batctl_settings_fragmentation,
 	      "[0|1]             \tdisplay or modify fragmentation setting");

gw_mode.c
@@ -392,6 +392,6 @@ out:
 	return res;
 }
 
-COMMAND(SUBCOMMAND, gw_mode, "gw",
+COMMAND(SUBCOMMAND_MIF, gw_mode, "gw",
 	COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK, NULL,
 	"[mode]            \tdisplay or modify the gateway mode");

commit 1d7dba848f64ffceec4ad1df2deb0055f57602b0

batctl: Fix command restriction for meshif-only prefix
The allowed commands for a meshif prefix are evaluated by comparing the
type bits. These are initialized completely when allowing non-meshif and
meshif commands. But a meshif-only prefix did an OR without previously
setting the types bitfield to 0. This could have allowed too many commands
than expected.

Fixes: 23f39261428e ("batctl: Replace '-m meshif' option with selector prefix")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

main.c
@@ -159,7 +159,7 @@ static const struct command *find_command_by_types(uint32_t types,
 
 static const struct command *find_command(struct state *state, const char *name)
 {
-	uint32_t types;
+	uint32_t types = 0;
 
 	switch (state->selector) {
 	case SP_NONE_OR_MESHIF:
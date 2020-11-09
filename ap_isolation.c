open-mesh-mirror/batctl

commit c56a63a5f12ab5b3b2ad8558b50c0e420013f7f8

batctl: Support generic netlink for ap_isolation command
sysfs should be avoided for new settings of network interfaces. To still
provide a common configuration infrastructure, all the existing settings
subcommands also have to be reimplemented via generic netlink while still
using sysfs as fallback.

The ap_isolation implementation is using the commands
BATADV_CMD_SET_MESH/BATADV_CMD_GET_MESH and
BATADV_CMD_SET_VLAN/BATADV_CMD_GET_VLAN to set/get the configuration of the
u8 (boolean) BATADV_ATTR_AP_ISOLATION_ENABLED attribute.

Signed-off-by: Sven Eckelmann <sven@narfation.org>

ap_isolation.c
@@ -21,13 +21,77 @@
  */
 
 #include "main.h"
+
+#include <errno.h>
+#include <linux/genetlink.h>
+#include <netlink/genl/genl.h>
+
+#include "batman_adv.h"
+#include "netlink.h"
 #include "sys.h"
+
+static struct simple_boolean_data ap_isolation;
+
+static int print_ap_isolation(struct nl_msg *msg, void *arg)
+{
+	return sys_simple_print_boolean(msg, arg,
+					BATADV_ATTR_AP_ISOLATION_ENABLED);
+}
+
+static int get_attrs_ap_isolation(struct nl_msg *msg, void *arg)
+{
+	struct state *state = arg;
+
+	if (state->vid >= 0)
+		nla_put_u16(msg, BATADV_ATTR_VLANID, state->vid);
+
+	return 0;
+}
+
+static int get_ap_isolation(struct state *state)
+{
+	enum batadv_nl_commands nl_cmd = BATADV_CMD_SET_MESH;
+
+	if (state->vid >= 0)
+		nl_cmd = BATADV_CMD_GET_VLAN;
+
+	return sys_simple_nlquery(state, nl_cmd, get_attrs_ap_isolation,
+				  print_ap_isolation);
+}
+
+static int set_attrs_ap_isolation(struct nl_msg *msg, void *arg)
+{
+	struct state *state = arg;
+	struct settings_data *settings = state->cmd->arg;
+	struct simple_boolean_data *data = settings->data;
+
+	nla_put_u8(msg, BATADV_ATTR_AP_ISOLATION_ENABLED, data->val);
+
+	if (state->vid >= 0)
+		nla_put_u16(msg, BATADV_ATTR_VLANID, state->vid);
+
+	return 0;
+}
+
+static int set_ap_isolation(struct state *state)
+{
+	enum batadv_nl_commands nl_cmd = BATADV_CMD_SET_MESH;
+
+	if (state->vid >= 0)
+		nl_cmd = BATADV_CMD_SET_VLAN;
+
+	return sys_simple_nlquery(state, nl_cmd, set_attrs_ap_isolation, NULL);
+}
 
 static struct settings_data batctl_settings_ap_isolation = {
 	.sysfs_name = "ap_isolation",
-	.params = sysfs_param_enable,
+	.data = &ap_isolation,
+	.parse = parse_simple_boolean,
+	.netlink_get = get_ap_isolation,
+	.netlink_set = set_ap_isolation,
 };
 
 COMMAND_NAMED(SUBCOMMAND, ap_isolation, "ap", handle_sys_setting,
-	      COMMAND_FLAG_MESH_IFACE, &batctl_settings_ap_isolation,
+	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
+	      &batctl_settings_ap_isolation,
 	      "[0|1]             \tdisplay or modify ap_isolation setting");

commit 107cee536a0f8024208923f49a2a548b40bfc432

batctl: Fix retrieval of meshif ap_isolation
The batadv command to retrieve the attributes is called
BATADV_CMD_GET_MESH. The used BATADV_CMD_SET_MESH will only return the
current settings via the "config" multicast group which is not evaluated by
the ap_isolation command.

Fixes: c56a63a5f12a ("batctl: Support generic netlink for ap_isolation command")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

ap_isolation.c
@@ -36,7 +36,7 @@ static int get_attrs_ap_isolation(struct nl_msg *msg, void *arg)
 
 static int get_ap_isolation(struct state *state)
 {
-	enum batadv_nl_commands nl_cmd = BATADV_CMD_SET_MESH;
+	enum batadv_nl_commands nl_cmd = BATADV_CMD_GET_MESH;
 
 	if (state->selector == SP_VLAN)
 		nl_cmd = BATADV_CMD_GET_VLAN;
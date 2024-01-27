open-mesh-mirror/batctl

commit 3bdfc388e74b7733b8a0cb6604baef4b43b26e94

implement simple tcpdump, first only batman packets

tcpdump.c
@@ -1,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <net/if.h>
+#include <netpacket/packet.h>
+#include <sys/ioctl.h>
+#include <netinet/ether.h>
+
+#include "battool.h"
+
+
+void tcpdump_usage() {
+	printf("Battool module tcpdump\n");
+	printf("Usage: battool tcpdump|td interface\n");
+	printf("\t-h help\n");
+	return;
+}
+
+void print_batman_packet( unsigned char *buf)
+{
+	struct batman_packet *bp = (struct batman_packet *)buf;
+	printf("batman header:\n");
+	printf(" ptype=%d flags=%x ttl=%u orig=%s seq=%u gwflags=%x version=%u\n", bp->packet_type,bp->flags, bp->ttl, ether_ntoa((struct ether_addr*) bp->orig), ntohs(bp->seqno), bp->gwflags, bp->version );
+}
+
+void print_packet( int length, unsigned char *buf )
+{
+	int i = 0;
+	printf("\n");
+	for( ; i < length; i++ ) {
+		if( i == 0 )
+			printf("0000| ");
+
+		if( i != 0 && i%8 == 0 )
+			printf("  ");
+		if( i != 0 && i%16 == 0 )
+			printf("\n%04d| ", i/16*10);
+
+		printf("%02x ", buf[i] );
+	}
+	printf("\n");
+}
+
+int tcpdump_main( int argc, char **argv )
+{
+	int  optchar,
+		packetsize = 2000;
+
+	unsigned char packet[packetsize];
+	uint8_t found_args = 1;
+	int32_t rawsock;
+	ssize_t rec_length;
+
+	struct ether_header *eth = (struct ether_header *) packet;
+	struct ifreq req;
+	struct sockaddr_ll addr;
+
+	char *devicename;
+
+	while ( ( optchar = getopt ( argc, argv, "h" ) ) != -1 ) {
+		switch( optchar ) {
+			case 'h':
+				tcpdump_usage();
+				exit(EXIT_SUCCESS);
+				break;
+			default:
+				tcpdump_usage();
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if ( argc <= found_args ) {
+		tcpdump_usage();
+		exit(EXIT_FAILURE);
+	}
+
+	devicename = argv[found_args];
+
+	if ( ( rawsock = socket(PF_PACKET,SOCK_RAW,htons( 0x0842 ) ) ) < 0 ) {
+		printf("Error - can't create raw socket: %s\n", strerror(errno) );
+		exit( EXIT_FAILURE );
+	}
+
+	strncpy(req.ifr_name, devicename, IFNAMSIZ);
+
+	if ( ioctl(rawsock, SIOCGIFINDEX, &req) < 0 ) {
+		printf("Error - can't create raw socket (SIOCGIFINDEX): %s\n", strerror(errno) );
+		exit( EXIT_FAILURE );
+	}
+
+	addr.sll_family   = AF_PACKET;
+	addr.sll_protocol = htons( 0x0842 );
+	addr.sll_ifindex  = req.ifr_ifindex;
+
+	if ( bind(rawsock, (struct sockaddr *)&addr, sizeof(addr)) < 0 ) {
+		printf( "Error - can't bind raw socket: %s\n", strerror(errno) );
+		close(rawsock);
+		exit( EXIT_FAILURE );
+	}
+
+	while( ( rec_length = read(rawsock,packet,packetsize) ) > 0 ) {
+
+		printf("\n---------------------------------------------------------------------------------------\n\n");
+		printf("ethernet header:\n");
+		printf(" %d bytes dest=%s ", rec_length, ether_ntoa( (struct ether_addr *)eth->ether_dhost ) );
+		printf("src=%s type=%04x\n", ether_ntoa( (struct ether_addr *) eth->ether_shost ),  ntohs( eth->ether_type ) );
+
+		if( packet[sizeof( struct ether_header)] == 0 )
+			print_batman_packet( packet + sizeof( struct ether_header ) );
+		else
+			printf("message typ %d currently not supported\n", packet[sizeof( struct ether_header)] );
+
+		print_packet( rec_length, packet );
+
+	}
+
+	exit( EXIT_SUCCESS );
+}

Makefile
@@ -92,7 +92,7 @@ all:		$(BINARY_NAME)
 
 
 
-battool: battool.o ping.o functions.o traceroute.o
+battool: battool.o ping.o functions.o traceroute.o tcpdump.o
 
 battool.o: battool.c battool.h
 
@@ -102,6 +102,7 @@ ping.o: ping.c
 
 traceroute.o: traceroute.c
 
+tcpdump.o: tcpdump.c
 
 
 

battool.c
@@ -31,7 +31,7 @@
 
 void usage() {
 	printf("Usage:\n\tbattool -v\n\tbattool module [options] destination\n");
-	printf("module: ping|p traceroute|t\n");
+	printf("module: ping|p traceroute|tr tcpdump|td\n");
 	printf("Use \"battool module -h\" for available options\n");
 	exit(EXIT_FAILURE);
 }
@@ -66,7 +66,7 @@ void parse_hosts_file( struct hosts **tmp, char path[] ) {
 }
 
 int main( int argc, char **argv ) {
-
+	int uid;
 	if( strcmp( argv[1], "-v" ) == 0 ) {
 		printf("Battool %s\n", VERSION);
 		exit(EXIT_SUCCESS);
@@ -76,6 +76,9 @@ int main( int argc, char **argv ) {
 		usage();
 	}
 
+	uid = getuid();
+	if(uid != 0) { printf("You must have UID 0 instead of %d.\n",uid); exit(EXIT_FAILURE); }
+
 	struct hosts *hosts = NULL;
 	parse_hosts_file( &hosts,HOSTS_FILE );
 
@@ -83,9 +86,13 @@ int main( int argc, char **argv ) {
 		/* call ping main function */
 		return ( ping_main( argc-1, argv+1, hosts ) );
 
-	} else if( strcmp(argv[1], "traceroute") == 0 || strcmp(argv[1], "t") == 0  ) {
+	} else if( strcmp(argv[1], "traceroute") == 0 || strcmp(argv[1], "tr") == 0  ) {
 		/* call trace main function */
 		return ( traceroute_main( argc-1, argv+1, hosts ) );
+
+	} else if( strcmp(argv[1], "tcpdump") == 0 || strcmp(argv[1], "td") == 0  ) {
+		/* call trace main function */
+		return ( tcpdump_main( argc-1, argv+1 ) );
 
 	} else {
 

traceroute.c
@@ -39,7 +39,7 @@
 
 void traceroute_usage() {
 	printf("Battool module traceroute\n");
-	printf("Usage: battool traceroute|t [options] destination\n");
+	printf("Usage: battool traceroute|tr [options] destination\n");
 	printf("\t-h help\n");
 	printf("destination:\n\t00:0a:00:93:d0:cf can write :a::93:d0:cf\n\tor use bat-hosts file\n");
 	return;

commit 40c4a30e57f99632d1355a83dbd59228651562d6

batctl: tcpdump: Fix missing sanity check for batman-adv header
parse_eth_hdr() is assuming that every ETH_P_BATMAN ethernet packet has a
valid, minimal batman-adv header (packet_type, version, ttl) attached. But
it doesn't actually check if the received buffer has enough bytes to access
the two bytes packet_type + version. So it is possible that it tries to
read outside of the received data.

Fixes: 3bdfc388e74b ("implement simple tcpdump, first only batman packets")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

tcpdump.c
@@ -1167,6 +1167,9 @@ static void parse_eth_hdr(unsigned char *packet_buff, ssize_t buff_len, int read
 			dump_vlan(packet_buff, buff_len, read_opt, time_printed);
 		break;
 	case ETH_P_BATMAN:
+		/* check for batman-adv packet_type + version */
+		LEN_CHECK(buff_len, sizeof(*eth_hdr) + 2, "BAT HEADER")
+
 		batman_ogm_packet = (struct batadv_ogm_packet *)(packet_buff + ETH_HLEN);
 
 		if ((read_opt & COMPAT_FILTER) &&
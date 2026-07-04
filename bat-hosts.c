open-mesh-mirror/batctl

commit db97c2a77e81de3903a5a4d68627bd4bedd7dcbe

batctl: fix crash in bat-hosts parser on embedded systems
The bat-hosts parser uses the realpath() function to recognize already
parsed files. On most systems realpath() allocates a buffer containing
the file's location. Some embedded libc implementations seem to expect
an allocated  buffer as second argument otherwise they simply crash.
batctl now always allocates a buffer to handle this case.

Reported-by: Linus Luessing <linus.luessing@web.de>
Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>

bat-hosts.c
@@ -150,8 +150,19 @@ void bat_hosts_init(void)
 	char confdir[CONF_DIR_LEN];
 	char *homedir;
 	size_t locations = sizeof(bat_hosts_path) / sizeof(char *);
-	char *normalized[locations];
+	char *normalized;
 
+	/***
+	 * realpath could allocate the memory for us but some embedded libc
+	 * implementations seem to expect a buffer as second argument
+	 */
+	normalized = malloc(locations * PATH_MAX);
+	if (!normalized) {
+		printf("Warning - couldn't not get memory for bat-hosts file parsing\n");
+		return;
+	}
+
+	memset(normalized, 0, locations * PATH_MAX);
 	host_hash = hash_new(64, compare_mac, choose_mac);
 
 	if (!host_hash) {
@@ -174,30 +185,23 @@ void bat_hosts_init(void)
 			confdir[CONF_DIR_LEN - 1] = '\0';
 		}
 
-		normalized[i] = realpath(confdir, NULL);
-		if (normalized[i] == NULL)
+		if (!realpath(confdir, normalized + (i * PATH_MAX)))
 			continue;
 
 		/* check for duplicates: don't parse the same file twice */
 		parse = 1;
 		for (j = 0; j < i; j++) {
-			if (normalized[j] == NULL)
-				continue;
-
-			if (strncmp(normalized[i], normalized[j], CONF_DIR_LEN) == 0) {
+			if (strncmp(normalized + (i * PATH_MAX), normalized + (j * PATH_MAX), CONF_DIR_LEN) == 0) {
 				parse = 0;
 				break;
 			}
 		}
 
-		if (parse && (normalized[i] != NULL))
-			parse_hosts_file(&host_hash, normalized[i]);
+		if (parse)
+			parse_hosts_file(&host_hash, normalized + (i * PATH_MAX));
 	}
 
-	for (i = 0; i < locations; i++) {
-		if (normalized[i])
-			free(normalized[i]);
-	}
+	free(normalized);
 }
 
 struct bat_host *bat_hosts_find_by_name(char *name)

commit 65d8c6d78d745264a2bb49fa79baaa8865e6d5f7

batctl: bat-hosts: compare full path for deduplication
The deduplication code uses realpath to store the real path of a provided
configuration file. The code then tries to compare these paths but
accidentally only comparse only the first CONF_DIR_LEN (256) bytes of the
path.

The number of bytes must be set to the same length as the path size slot in
the normalized buffer.

Fixes: db97c2a77e81 ("batctl: fix crash in bat-hosts parser on embedded systems")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

bat-hosts.c
@@ -203,7 +203,7 @@ void bat_hosts_init(int read_opt)
 		for (j = 0; j < i; j++) {
 			if (strncmp(normalized + (i * PATH_MAX),
 				    normalized + (j * PATH_MAX),
-				    CONF_DIR_LEN) == 0) {
+				    PATH_MAX) == 0) {
 				parse = 0;
 				break;
 			}
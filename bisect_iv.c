open-mesh-mirror/batctl

commit 87ade2869cf38f1a15e9507a664e303de7fe16f7

add interval and loop count options

ping.c
@@ -27,19 +27,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #include "battool.h"
+#include "functions.h"
+
+#define VERSION "0.1 alpha"
 
 void ping_usage() {
+	printf("Battool module ping\n");
+	printf("Usage: battool ping [options] destination\n");
+	printf("\t-c count\n");
+	printf("\t-h help\n");
+	printf("\t-i interval in seconds\n");
+	printf("\t-v version\n");
+	printf("destination: 00:0a:00:93:d0:cf can write :a::93:d0:cf\n");
 	return;
 }
 
-void ping_main( uint8_t *mac, char *mac_string ) {
+int ping_main( int argc, char **argv ) {
 
 	char *send_buff, *rec_buff;
 	char begin[] = "p:";
 	int sbsize,rbsize;
-	uint8_t res;
+	uint8_t res, mac[6];
 	int32_t recv_buff_len;
 	struct icmp_packet icmp_packet;
 	struct unix_if unix_if;
@@ -52,7 +63,51 @@ void ping_main( uint8_t *mac, char *mac_string ) {
 	int trans=0, recv=0, avg_count=0;
 	float min= -1.0, avg=0.0, max=0.0;
 
+	int optchar;
+	uint8_t found_args = 1;
+	uint8_t loop = 0;
+	int loop_count = 0;
+	int loop_interval = 0;
+	char *mac_string;
+
+	while ( ( optchar = getopt ( argc, argv, "hvc:i:" ) ) != -1 ) {
+		switch( optchar ) {
+			case 'h':
+				ping_usage();
+				exit(EXIT_SUCCESS);
+				break;
+			case 'v':
+				printf("Battool module ping %s\n", VERSION);
+				exit(EXIT_SUCCESS);
+				break;
+			case 'c':
+				loop_count = strtol(optarg, NULL , 10);
+				loop = 1;
+				found_args+=2;
+				break;
+			case 'i':
+				loop_interval = strtol(optarg, NULL , 10);
+				found_args+=2;
+				break;
+			default:
+				ping_usage();
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if ( argc <= found_args ) {
+		ping_usage();
+		exit(EXIT_FAILURE);
+	}
 	
+	mac_string = argv[found_args];
+
+	
+	if( convert_mac( mac_string, mac ) < 1 ) {
+		printf("The mac address was not correct.\n");
+		exit(EXIT_FAILURE);
+	}
+
 	sbsize = sizeof( struct icmp_packet ) + 2;
 	rbsize = sizeof( struct icmp_packet );
 
@@ -86,6 +141,12 @@ void ping_main( uint8_t *mac, char *mac_string ) {
 	memcpy( send_buff, begin, 2 );
 	printf("PING %s\n", mac_string );
 	while( !Stop ) {
+		if( loop ) {
+			if( loop_count )
+				loop_count--;
+			else
+				break;
+		}
 
 		icmp_packet.seqno++;
 		memcpy( send_buff+2, &icmp_packet, rbsize );
@@ -108,7 +169,7 @@ void ping_main( uint8_t *mac, char *mac_string ) {
 
 		if( res > 0 )
 		{
-			
+	
 			if ( ( recv_buff_len = read( unix_if.unix_sock, rec_buff, rbsize ) ) > 0 )
 			{
 				gettimeofday(&end,(struct timezone*)0);
@@ -133,7 +194,6 @@ void ping_main( uint8_t *mac, char *mac_string ) {
 					time_delta = (double)usec/1000;
 					printf("%d bytes from %s icmp_seq=%d ttl=%d time=%.2f ms\n",recv_buff_len, mac_string, ((struct icmp_packet *)rec_buff)->seqno,((struct icmp_packet *)rec_buff)->ttl, time_delta );
 
-					/* statistic */
 					if( time_delta < min || min == -1.0 ) min = time_delta;
 					if( time_delta > max ) max = time_delta;
 					avg += time_delta;
@@ -151,10 +211,10 @@ void ping_main( uint8_t *mac, char *mac_string ) {
 		} else if ( res == 0 ) {
 			printf("Host %s timeout\n",mac_string );
 		}
-		sleep(1);
+		sleep( loop_interval?loop_interval:1 );
 	}
 	printf("--- %s ping statistic ---\n",mac_string );
 	printf("%d packets transmitted, %d received, %d%c packet loss\n", trans, recv, ( (trans - recv) * 100 / trans ),'%');
 	printf("rtt min/avg/max/mdev = %.3f/%.3f/%.3f/%.3f ms\n", min, (avg / avg_count),max, max-min );
-	return;
+	return 1;
 }

Makefile
@@ -1,9 +1,11 @@
 CC=gcc
 CFLAGS= -Wall -O0 -g3
 
-battool: battool.o ping.o
+battool: battool.o ping.o functions.o
 
 battool.o: battool.c battool.h
+
+functions.o: functions.c functions.h
 
 ping.o: ping.c battool.h
 

battool.c
@@ -29,13 +29,12 @@
 
 #include "battool.h"
 
-
 uint8_t Stop = 0;
 
 void usage() {
-	printf("Usage: battool modus destination\n");
+	printf("Usage: battool modus [options] destination\n");
 	printf("modus: ping\n");
-	printf("destination: 00:0a:00:93:d0:cf can write :a::93:d0:cf\n\n");
+	printf("Use \"battool modus -h\" for available options\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -52,71 +51,22 @@ void handler( int32_t sig ) {
 }
 
 int main( int argc, char **argv ) {
-	uint8_t mac[6];
-	char tmp[2];
-	int i,j=0;
 
 	if( argc < 3 ) {
 		usage();
 	}
 	
-	
 	signal( SIGINT, handler );
 	signal( SIGTERM, handler );
-	
-	if( argc == 3 ) {
 
-		if( strcmp(argv[2], "help") != 0 ) {
-			/* convert mac address in int array */
-			if( strlen( argv[2] ) > 17 ) {
-				printf("The mac address was not correct.\n");
-				exit(EXIT_FAILURE);
-			}
-
-			for( i = 0; i < strlen( argv[2] ) ; ) {
-				if( argv[2][i] != ':' ) {
-					tmp[0] = argv[2][i];
-				} else {
-					mac[j] = 0;
-					i++;
-					j++;
-					continue;
-				}
-
-				if( argv[2][i+1] != ':' ) {
-					tmp[1] = argv[2][i+1];
-					i+=3;
-				} else {
-					tmp[1] = tmp[0];
-					tmp[0] = '0';
-					i+=2;
-				}
-
-				mac[j] = strtol(tmp,NULL,16);
-				j++;
-			}
-
-			if( j < 5 || j > 6 ) {
-				printf("The mac address was not correct.\n");
-				exit(EXIT_FAILURE);
-			}
-
-		} else {
-			/* print help for modus in argv[1] */
-		}
-
-		if( strcmp(argv[1], "ping") == 0 ) {
-			/* call ping main function */
-			ping_main( mac, argv[2] );
-
-		} else {
-			usage();
-		}
+	if( strcmp(argv[1], "ping") == 0 ) {
+		/* call ping main function */
+		return ( ping_main( argc-1, argv+1 ) );
 
 	} else {
-		printf("more options currently not supported\n");
+
 		usage();
-		exit(EXIT_FAILURE);
+
 	}
 
 

battool.h
@@ -27,6 +27,6 @@ struct unix_if {
 	struct sockaddr_un addr;
 };
 
-void ping_main( uint8_t *mac,char *mac_string );
+int ping_main( int argc, char **argv );
 
 extern uint8_t Stop;

commit 26b997032052b0d7ab842a156f65fab5441581a7

batctl: fix parsing of parameters with arguments
If using a getopt argument with an argument, it can be supplied via:

* -m asd
* -m=asd
* -masd

Only the first version is correctly handled by the found_args
implementation (without causing undefined behavior). But it can be instead
simplified and fixed at the same time by directly using the optind variable
from getopt.

Fixes: 87ade2869cf3 ("add interval and loop count options")
Fixes: 3bdfc388e74b ("implement simple tcpdump, first only batman packets")
Fixes: ece05e1c4c1f ("[batctl] bisect (a tool to analyze logfiles) added")
Fixes: f3c9cf9e730e ("source out calculation of round trip time to functions.c add abbreviation for modules, example ping = p add traceroute module, not complete")
Fixes: f109b3473f86 ("batctl: introduce throughput meter support")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

bisect_iv.c
@@ -1495,7 +1495,6 @@ static int bisect_iv(struct state *state __maybe_unused, int argc, char **argv)
 	int num_parsed_files;
 	long long tmp_seqno;
 	char orig[NAME_LEN];
-	int found_args = 1;
 	char *dash_ptr;
 	int optchar;
 	int res;
@@ -1510,19 +1509,15 @@ static int bisect_iv(struct state *state __maybe_unused, int argc, char **argv)
 			return EXIT_SUCCESS;
 		case 'l':
 			loop_orig_ptr = optarg;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 'n':
 			read_opt &= ~USE_BAT_HOSTS;
-			found_args += 1;
 			break;
 		case 'o':
 			filter_orig_ptr = optarg;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 'r':
 			rt_orig_ptr = optarg;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 's':
 			dash_ptr = strchr(optarg, '-');
@@ -1549,11 +1544,9 @@ static int bisect_iv(struct state *state __maybe_unused, int argc, char **argv)
 				*dash_ptr = '-';
 			}
 
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 't':
 			trace_orig_ptr = optarg;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		default:
 			bisect_iv_usage();
@@ -1561,7 +1554,7 @@ static int bisect_iv(struct state *state __maybe_unused, int argc, char **argv)
 		}
 	}
 
-	if (argc <= found_args + 1) {
+	if (argc <= optind + 1) {
 		fprintf(stderr, "Error - need at least 2 log files to compare\n");
 		bisect_iv_usage();
 		goto err;
@@ -1624,13 +1617,13 @@ static int bisect_iv(struct state *state __maybe_unused, int argc, char **argv)
 			goto err;
 	}
 
-	while (argc > found_args) {
-		res = parse_log_file(argv[found_args]);
+	while (argc > optind) {
+		res = parse_log_file(argv[optind]);
 
 		if (res > 0)
 			num_parsed_files++;
 
-		found_args++;
+		optind++;
 	}
 
 	if (num_parsed_files < 2) {

main.c
@@ -357,7 +357,7 @@ int main(int argc, char **argv)
 			fprintf(stderr,
 				"Warning - option -m was deprecated and will be removed in the future\n");
 
-			state.arg_iface = argv[2];
+			state.arg_iface = optarg;
 			break;
 		case 'v':
 			version();

ping.c
@@ -75,7 +75,6 @@ static int ping(struct state *state, int argc, char **argv)
 	uint8_t last_rr_cur = 0;
 	int ret = EXIT_FAILURE;
 	int loop_count = -1;
-	int found_args = 1;
 	size_t packet_len;
 	struct timeval tv;
 	double time_delta;
@@ -100,7 +99,6 @@ static int ping(struct state *state, int argc, char **argv)
 			loop_count = strtol(optarg, NULL, 10);
 			if (loop_count < 1)
 				loop_count = -1;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 'h':
 			ping_usage();
@@ -117,21 +115,17 @@ static int ping(struct state *state, int argc, char **argv)
 			fractional_part = modf(ping_interval, &integral_part);
 			loop_interval.tv_sec = (time_t)integral_part;
 			loop_interval.tv_nsec = (long)(fractional_part * 1000000000l);
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 't':
 			timeout = strtol(optarg, NULL, 10);
 			if (timeout < 1)
 				timeout = 1;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 'R':
 			rr = 1;
-			found_args++;
 			break;
 		case 'T':
 			disable_translate_mac = 1;
-			found_args += 1;
 			break;
 		default:
 			ping_usage();
@@ -139,13 +133,13 @@ static int ping(struct state *state, int argc, char **argv)
 		}
 	}
 
-	if (argc <= found_args) {
+	if (optind >= argc) {
 		fprintf(stderr, "Error - target mac address or bat-host name not specified\n");
 		ping_usage();
 		return EXIT_FAILURE;
 	}
 
-	dst_string = argv[found_args];
+	dst_string = argv[optind];
 	bat_hosts_init(0);
 	bat_host = bat_hosts_find_by_name(dst_string);
 

tcpdump.c
@@ -1493,7 +1493,6 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 	fd_set tmp_wait_sockets;
 	int ret = EXIT_FAILURE;
 	fd_set wait_sockets;
-	int found_args = 1;
 	struct timeval tv;
 	int max_sock = 0;
 	ssize_t read_len;
@@ -1507,26 +1506,22 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 		switch (optchar) {
 		case 'c':
 			read_opt |= COMPAT_FILTER;
-			found_args += 1;
 			break;
 		case 'h':
 			tcpdump_usage();
 			return EXIT_SUCCESS;
 		case 'n':
 			read_opt &= ~USE_BAT_HOSTS;
-			found_args += 1;
 			break;
 		case 'p':
 			tmp = strtol(optarg, NULL, 10);
 			if (tmp > 0 && tmp <= dump_level_all)
 				dump_level = tmp;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		case 'x':
 			tmp = strtol(optarg, NULL, 10);
 			if (tmp > 0 && tmp <= dump_level_all)
 				dump_level &= ~tmp;
-			found_args += ((*((char *)(optarg - 1)) == optchar) ? 1 : 2);
 			break;
 		default:
 			tcpdump_usage();
@@ -1534,7 +1529,7 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 		}
 	}
 
-	if (argc <= found_args) {
+	if (optind >= argc) {
 		fprintf(stderr, "Error - target interface not specified\n");
 		tcpdump_usage();
 		return EXIT_FAILURE;
@@ -1549,8 +1544,8 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 	INIT_LIST_HEAD(&dump_if_list);
 	FD_ZERO(&wait_sockets);
 
-	while (argc > found_args) {
-		dump_if = create_dump_interface(argv[found_args]);
+	while (optind < argc) {
+		dump_if = create_dump_interface(argv[optind]);
 		if (!dump_if)
 			goto out;
 
@@ -1559,7 +1554,7 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 
 		FD_SET(dump_if->raw_sock, &wait_sockets);
 		list_add_tail(&dump_if->list, &dump_if_list);
-		found_args++;
+		optind++;
 	}
 
 	while (!is_aborted) {

throughputmeter.c
@@ -326,7 +326,6 @@ static int throughputmeter(struct state *state, int argc, char **argv)
 	struct bat_host *bat_host;
 	int ret = EXIT_FAILURE;
 	uint64_t throughput;
-	int found_args = 1;
 	uint32_t time = 0;
 	char *dst_string;
 	int optchar;
@@ -334,12 +333,10 @@ static int throughputmeter(struct state *state, int argc, char **argv)
 	while ((optchar = getopt(argc, argv, "t:n")) != -1) {
 		switch (optchar) {
 		case 't':
-			found_args += 2;
 			time = strtoul(optarg, NULL, 10);
 			break;
 		case 'n':
 			read_opt &= ~USE_BAT_HOSTS;
-			found_args += 1;
 			break;
 		default:
 			tp_meter_usage();
@@ -347,12 +344,12 @@ static int throughputmeter(struct state *state, int argc, char **argv)
 		}
 	}
 
-	if (argc <= found_args) {
+	if (optind >= argc) {
 		tp_meter_usage();
 		return EXIT_FAILURE;
 	}
 
-	dst_string = argv[found_args];
+	dst_string = argv[optind];
 	bat_hosts_init(read_opt);
 	bat_host = bat_hosts_find_by_name(dst_string);
 

traceroute.c
@@ -48,7 +48,6 @@ static int traceroute(struct state *state, int argc, char **argv)
 	int ret = EXIT_FAILURE;
 	char dst_reached = 0;
 	int seq_counter = 0;
-	int found_args = 1;
 	struct timeval tv;
 	ssize_t read_len;
 	char *dst_string;
@@ -65,11 +64,9 @@ static int traceroute(struct state *state, int argc, char **argv)
 			return EXIT_SUCCESS;
 		case 'n':
 			read_opt &= ~USE_BAT_HOSTS;
-			found_args += 1;
 			break;
 		case 'T':
 			disable_translate_mac = 1;
-			found_args += 1;
 			break;
 		default:
 			traceroute_usage();
@@ -77,13 +74,13 @@ static int traceroute(struct state *state, int argc, char **argv)
 		}
 	}
 
-	if (argc <= found_args) {
+	if (optind >= argc) {
 		fprintf(stderr, "Error - target mac address or bat-host name not specified\n");
 		traceroute_usage();
 		return EXIT_FAILURE;
 	}
 
-	dst_string = argv[found_args];
+	dst_string = argv[optind];
 	bat_hosts_init(read_opt);
 	bat_host = bat_hosts_find_by_name(dst_string);
 
open-mesh-mirror/batctl

commit 7738b534fb071290e23ae2b8565d85ba1409ee62

batctl: tcpdump - add vlan support
This patch let's the tcpdump component decompose & display vlan
(802.1Q) packets.

tcpdump.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
  *
  * Andreas Langer <a.langer@q-dsl.de>
@@ -221,6 +221,34 @@ void dump_ip(unsigned char *packet_buff, ssize_t buff_len, int time_printed)
 	}
 }
 
+void dump_vlan(unsigned char *packet_buff, ssize_t buff_len, int time_printed)
+{
+	struct vlanhdr *vlanhdr;
+
+	vlanhdr = (struct vlanhdr *)packet_buff;
+	LEN_CHECK((size_t)buff_len, sizeof(struct vlanhdr), "VLAN");
+
+	if (!time_printed)
+		print_time();
+
+	vlanhdr->vid = ntohs(vlanhdr->vid);
+	printf("vlan %u, p %u, ", vlanhdr->vid, vlanhdr->vid >> 12);
+
+	switch (ntohs(vlanhdr->ether_type)) {
+	case ETH_P_ARP:
+		dump_arp(packet_buff + sizeof(struct vlanhdr),
+			 buff_len - sizeof(struct vlanhdr), 1);
+		break;
+	case ETH_P_IP:
+		dump_ip(packet_buff + sizeof(struct vlanhdr),
+			buff_len - sizeof(struct vlanhdr), 1);
+		break;
+	default:
+		printf(" unknown payload ether type: 0x%04x\n", ntohs(vlanhdr->ether_type));
+		break;
+	}
+}
+
 void dump_batman_ogm(unsigned char *packet_buff, ssize_t buff_len, int read_opt)
 {
 	struct ether_header *ether_header;
@@ -320,8 +348,12 @@ void dump_batman_ucast(unsigned char *packet_buff, ssize_t buff_len, int read_op
 		dump_ip(packet_buff + (2 * sizeof(struct ether_header)) + sizeof(struct unicast_packet),
 			buff_len - (2 * sizeof(struct ether_header)) - sizeof(struct unicast_packet), 1);
 		break;
+	case ETH_P_8021Q:
+		dump_vlan(packet_buff + (2 * sizeof(struct ether_header)) + sizeof(struct unicast_packet),
+			  buff_len - (2 * sizeof(struct ether_header)) - sizeof(struct unicast_packet), 1);
+		break;
 	default:
-		printf(" unknown payload ether type: %hu\n", ntohs(ether_header->ether_type));
+		printf(" unknown payload ether type: 0x%04x\n", ntohs(ether_header->ether_type));
 		break;
 	}
 }
@@ -358,8 +390,12 @@ void dump_batman_bcast(unsigned char *packet_buff, ssize_t buff_len, int read_op
 		dump_ip(packet_buff + (2 * sizeof(struct ether_header)) + sizeof(struct bcast_packet),
 			buff_len - (2 * sizeof(struct ether_header)) - sizeof(struct bcast_packet), 1);
 		break;
+	case ETH_P_8021Q:
+		dump_vlan(packet_buff + (2 * sizeof(struct ether_header)) + sizeof(struct bcast_packet),
+			  buff_len - (2 * sizeof(struct ether_header)) - sizeof(struct bcast_packet), 1);
+		break;
 	default:
-		printf(" unknown payload ether type: %hu\n", ntohs(ether_header->ether_type));
+		printf(" unknown payload ether type: 0x%04x\n", ntohs(ether_header->ether_type));
 		break;
 	}
 }
@@ -510,6 +546,11 @@ int tcpdump(int argc, char **argv)
 					dump_ip(packet_buff + sizeof(struct ether_header),
 						read_len - sizeof(struct ether_header), 0);
 				break;
+			case ETH_P_8021Q:
+				if (dump_level & DUMP_TYPE_NONBAT)
+					dump_vlan(packet_buff + sizeof(struct ether_header),
+						read_len - sizeof(struct ether_header), 0);
+				break;
 			case ETH_P_BATMAN:
 				batman_packet = (struct batman_packet *)(packet_buff + sizeof(struct ether_header));
 
@@ -538,7 +579,7 @@ int tcpdump(int argc, char **argv)
 
 				break;
 			default:
-				printf("Warning - packet contains unknown ether type: %hu\n", ether_type);
+				printf("Warning - packet contains unknown ether type: 0x%04x\n", ether_type);
 				break;
 			}
 

tcpdump.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
  *
  * Andreas Langer <a.langer@q-dsl.de>, Marek Lindner <lindner_marek@yahoo.de>
@@ -36,5 +36,10 @@ struct dump_if {
 	int32_t raw_sock;
 	struct sockaddr_ll addr;
 };
+
+struct vlanhdr {
+	unsigned short vid;
+	u_int16_t ether_type;
+} __attribute__ ((packed));
 
 int tcpdump(int argc, char **argv);

commit cd0ba25ad03a27226617678638ebb666b7f8eeb1

batctl: tcpdump: correct output of VLAN IDs
The 16 bit TCI header for VLAN tagging (IEEE 802.1Q) stores 12 bit for the
12 bit VID, 1 bit DEI and 3 bit PCP. It is therefore unexpected to print
the 16 bit as vlan identifier without masking out the upper 4 bit.

At the same time, also split the DEI ( Drop Eligible Indicator) and the PCP
(Priority Code Point) in separate output fields.

Fixes: 7738b534fb07 ("batctl: tcpdump - add vlan support")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

tcpdump.c
@@ -823,7 +823,8 @@ static void dump_vlan(unsigned char *packet_buff, ssize_t buff_len, int read_opt
 		time_printed = print_time();
 
 	vlanhdr->vid = ntohs(vlanhdr->vid);
-	printf("vlan %u, p %u, ", vlanhdr->vid, vlanhdr->vid >> 12);
+	printf("vlan %u, d %u, p %u, ", vlanhdr->vid & 0x0fff, (vlanhdr->vid >> 12) & 0x1,
+	       vlanhdr->vid >> 13);
 
 	/* overwrite vlan tags */
 	memmove(packet_buff + 4, packet_buff, 2 * ETH_ALEN);
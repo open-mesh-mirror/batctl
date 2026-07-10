open-mesh-mirror/batctl

commit c7c76f63c1f92f8a5263a48f25e8fe881a0754c1

[batctl] integrating batman hash implementation / move bat-hosts stuff into an extra set of files

bat-hosts.c
@@ -1,0 +1,144 @@
+/* Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
+ * Andreas Langer <a.langer@q-dsl.de>
+ * Marek Lindner <lindner_marek@yahoo.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ *
+ */
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include "main.h"
+#include "bat-hosts.h"
+#include "hash.h"
+
+
+struct hashtable_t *host_hash = NULL;
+
+
+static int compare_mac(void *data1, void *data2)
+{
+	return (memcmp(data1, data2, sizeof(struct ether_addr)) == 0 ? 1 : 0);
+}
+
+static int choose_mac(void *data, int32_t size)
+{
+	unsigned char *key= data;
+	uint32_t hash = 0, m_size = sizeof(struct ether_addr);
+	size_t i;
+
+	for (i = 0; i < m_size; i++) {
+		hash += key[i];
+		hash += (hash << 10);
+		hash ^= (hash >> 6);
+	}
+
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+
+	return (hash % size);
+}
+
+static void parse_hosts_file(struct hashtable_t *hash, char path[])
+{
+	FILE *fd;
+	char name[HOST_NAME_MAX_LEN], mac[18];
+	struct ether_addr *tmp_mac;
+	struct bat_host *bat_host;
+	struct hashtable_t *swaphash;
+
+	name[0] = mac[0] = '\0';
+
+	fd = fopen(path, "r");
+	if (fd == NULL)
+		return;
+
+	while (fscanf(fd,"%[^ \t]%s\n", name, mac) != EOF) {
+
+		tmp_mac = ether_aton(mac);
+		if (!tmp_mac) {
+			printf("Warning - invalid mac address in '%s' detected: %s\n", path, mac);
+			return;
+		}
+
+		bat_host = malloc(sizeof(struct bat_host));
+
+		if (!bat_host) {
+			printf("Error - could not allocate memory: %s\n", strerror(errno));
+			return;
+		}
+
+		memcpy(&bat_host->mac, tmp_mac, sizeof(struct ether_addr));
+		strncpy(bat_host->name, name, HOST_NAME_MAX_LEN - 1);
+
+		hash_add(hash, bat_host);
+
+		if (hash->elements * 4 > hash->size) {
+			swaphash = hash_resize(hash, hash->size * 2);
+
+			if (swaphash == NULL)
+				printf("Warning - couldn't resize bat hosts hash table\n");
+			else
+				hash = swaphash;
+		}
+
+	}
+
+	return;
+}
+
+int bat_hosts_init(void)
+{
+	host_hash = hash_new(64, compare_mac, choose_mac);
+
+	if (!host_hash)
+		return 0;
+
+	parse_hosts_file(host_hash, HOSTS_FILE);
+
+	return 1;
+}
+
+struct bat_host *bat_hosts_find_by_name(char *name)
+{
+	struct hash_it_t *hashit = NULL;
+	struct bat_host *bat_host = NULL, *tmp_bat_host;
+
+	while (NULL != (hashit = hash_iterate(host_hash, hashit))) {
+		tmp_bat_host = (struct bat_host *)hashit->bucket->data;
+
+		if (strcmp(tmp_bat_host->name, name) == 0)
+			bat_host = tmp_bat_host;
+			break;
+	}
+
+	return bat_host;
+}
+
+struct bat_host *bat_hosts_find_by_mac(char *mac)
+{
+	return (struct bat_host *)hash_find(host_hash, mac);
+}
+
+void bat_hosts_free(void)
+{
+	if (host_hash)
+		hash_destroy(host_hash);
+}

Makefile
@@ -25,40 +25,40 @@ ifndef V
 endif
 endif
 
-CC =		gcc
-CFLAGS +=	-pedantic -Wall -W -O1 -g3 -std=gnu99
-EXTRA_CFLAGS =	-DREVISION_VERSION=$(REVISION_VERSION)
+CC = gcc
+CFLAGS += -pedantic -Wall -W -O1 -g3 -std=gnu99
+EXTRA_CFLAGS = -DREVISION_VERSION=$(REVISION_VERSION)
 LDFLAGS +=
 
-SBINDIR =	$(INSTALL_PREFIX)/usr/sbin
+SBINDIR = $(INSTALL_PREFIX)/usr/sbin
 
 LOG_BRANCH = trunk/batctl
 
 SRC_FILES = "\(\.c\)\|\(\.h\)\|\(Makefile\)\|\(INSTALL\)\|\(LIESMICH\)\|\(README\)\|\(THANKS\)\|\(TRASH\)\|\(Doxyfile\)\|\(./posix\)\|\(./linux\)\|\(./bsd\)\|\(./man\)\|\(./doc\)"
 
-SRC_C = main.c functions.c batping.c batroute.c batdump.c list-batman.c hash.c
-SRC_H = main.h functions.h list-batman.h batdump.h hash.h
+SRC_C = main.c bat-hosts.c functions.c ping.c traceroute.c tcpdump.c list-batman.c hash.c
+SRC_H = main.h bat-hosts.h functions.h ping.h traceroute.h tcpdump.h list-batman.h hash.h allocate.h
 SRC_O = $(SRC_C:.c=.o)
 
-PACKAGE_NAME =	batctl
-BINARY_NAME =	batctl
+PACKAGE_NAME = batctl
+BINARY_NAME = batctl
 SOURCE_VERSION_HEADER = main.h
 
-REVISION =	$(shell if [ -d .svn ]; then \
-						if which svn > /dev/null; then \
-							svn info | grep "Rev:" | sed -e '1p' -n | awk '{print $$4}'; \
+REVISION = $(shell if [ -d .svn ]; then \
+					if which svn > /dev/null; then \
+						svn info | grep "Rev:" | sed -e '1p' -n | awk '{print $$4}'; \
+					else \
+						echo "[unknown]"; \
+					fi ; \
+				else \
+					if [ -d ~/.svk ]; then \
+						if which svk > /dev/null; then \
+							echo $$(svk info | grep "Mirrored From" | awk '{print $$5}'); \
 						else \
 							echo "[unknown]"; \
-						fi ; \
-					else \
-						if [ -d ~/.svk ]; then \
-							if which svk > /dev/null; then \
-								echo $$(svk info | grep "Mirrored From" | awk '{print $$5}'); \
-							else \
-								echo "[unknown]"; \
-							fi; \
 						fi; \
-					fi)
+					fi; \
+				fi)
 
 REVISION_VERSION =\"\ rv$(REVISION)\"
 

allocate.h
@@ -1,5 +1,6 @@
-/* Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
- * Andreas Langer <a.langer@q-dsl.de>
+/*
+ * Copyright (C) 2009 B.A.T.M.A.N. contributors:
+ *
  * Marek Lindner <lindner_marek@yahoo.de>
  *
  * This program is free software; you can redistribute it and/or
@@ -19,16 +20,14 @@
  */
 
 
-#include <sys/un.h>
+#ifndef _ALLOCATE_H
+#define _ALLOCATE_H 1
 
+/* debug allocate wrapper to keep hash.c happy */
 
-#define SOURCE_VERSION "0.2-beta"  /*put exactly one distinct word inside the string like "0.3-pre-alpha" or "0.3-rc1" or "0.3" */
+#include <stdlib.h>
 
-#define UNIX_PATH "/var/run/batmand-adv.socket"
-#define BAT_DEVICE "/dev/batman-adv"
+#define debugMalloc(length, tag) malloc(length)
+#define debugFree(mem, tag) free(mem)
 
-
-struct unix_if {
-	int unix_sock;
-	struct sockaddr_un addr;
-};
+#endif

bat-hosts.d
@@ -1,0 +1,32 @@
+bat-hosts.o: bat-hosts.c /usr/include/stdio.h /usr/include/features.h \
+  /usr/include/bits/predefs.h /usr/include/sys/cdefs.h \
+  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-32.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stddef.h \
+  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+  /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stdarg.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+  /usr/include/bits/stdio.h /usr/include/stdint.h \
+  /usr/include/bits/wchar.h /usr/include/errno.h \
+  /usr/include/bits/errno.h /usr/include/linux/errno.h \
+  /usr/include/asm/errno.h /usr/include/asm-generic/errno.h \
+  /usr/include/asm-generic/errno-base.h main.h /usr/include/sys/un.h \
+  /usr/include/bits/sockaddr.h /usr/include/string.h \
+  /usr/include/bits/string.h /usr/include/bits/string2.h \
+  /usr/include/endian.h /usr/include/bits/endian.h \
+  /usr/include/bits/byteswap.h /usr/include/stdlib.h bat-hosts.h \
+  /usr/include/netinet/ether.h /usr/include/netinet/if_ether.h \
+  /usr/include/sys/types.h /usr/include/time.h /usr/include/sys/select.h \
+  /usr/include/bits/select.h /usr/include/bits/sigset.h \
+  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+  /usr/include/bits/pthreadtypes.h /usr/include/linux/if_ether.h \
+  /usr/include/linux/types.h /usr/include/linux/posix_types.h \
+  /usr/include/linux/stddef.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include-fixed/asm/posix_types.h \
+  /usr/include/asm/posix_types.h /usr/include/asm/posix_types_32.h \
+  /usr/include/asm/types.h /usr/include/asm-generic/int-ll64.h \
+  /usr/include/net/ethernet.h /usr/include/net/if_arp.h \
+  /usr/include/sys/socket.h /usr/include/sys/uio.h \
+  /usr/include/bits/uio.h /usr/include/bits/socket.h \
+  /usr/include/asm/socket.h /usr/include/asm/sockios.h hash.h

bat-hosts.h
@@ -1,5 +1,4 @@
-/* Copyright (C) 2007-2009 B.A.T.M.A.N. contributors:
- * Andreas Langer <a.langer@q-dsl.de>
+/* Copyright (C) 2009 B.A.T.M.A.N. contributors:
  * Marek Lindner <lindner_marek@yahoo.de>
  *
  * This program is free software; you can redistribute it and/or
@@ -19,16 +18,21 @@
  */
 
 
-#include <sys/un.h>
+
+#include <netinet/ether.h>
 
 
-#define SOURCE_VERSION "0.2-beta"  /*put exactly one distinct word inside the string like "0.3-pre-alpha" or "0.3-rc1" or "0.3" */
+#define HOSTS_FILE "/etc/bat-hosts"
 
-#define UNIX_PATH "/var/run/batmand-adv.socket"
-#define BAT_DEVICE "/dev/batman-adv"
+#define HOST_NAME_MAX_LEN 50
 
 
-struct unix_if {
-	int unix_sock;
-	struct sockaddr_un addr;
-};
+struct bat_host {
+	struct ether_addr mac;
+	char name[HOST_NAME_MAX_LEN];
+} __attribute__((packed));
+
+int bat_hosts_init(void);
+struct bat_host *bat_hosts_find_by_name(char *name);
+struct bat_host *bat_hosts_find_by_mac(char *mac);
+void bat_hosts_free(void);

hash.c
@@ -1,0 +1,313 @@
+/*
+ * Copyright (C) 2006-2009 B.A.T.M.A.N. contributors:
+ *
+ * Simon Wunderlich, Marek Lindner
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ *
+ */
+
+
+#include <stdio.h>		/* NULL */
+#include "hash.h"
+#include "allocate.h"
+
+
+/* clears the hash */
+void hash_init(struct hashtable_t *hash) {
+	int i;
+	hash->elements=0;
+	for (i=0 ; i<hash->size ; i++) {
+		hash->table[i] = NULL;
+	}
+}
+
+
+/* remove the hash structure. if hashdata_free_cb != NULL,
+ * this function will be called to remove the elements inside of the hash.
+ * if you don't remove the elements, memory might be leaked. */
+void hash_delete(struct hashtable_t *hash, hashdata_free_cb free_cb) {
+	struct element_t *bucket, *last_bucket;
+	int i;
+
+	for (i=0; i<hash->size; i++) {
+
+		bucket= hash->table[i];
+		while (bucket != NULL) {
+
+			if (free_cb!=NULL)
+				free_cb( bucket->data );
+
+			last_bucket= bucket;
+			bucket= bucket->next;
+			debugFree(last_bucket, 1301);
+
+		}
+
+	}
+	hash_destroy(hash);
+}
+
+
+
+/* free only the hashtable and the hash itself. */
+void hash_destroy(struct hashtable_t *hash) {
+
+	debugFree( hash->table, 1302 );
+	debugFree( hash, 1303 );
+
+}
+
+
+
+
+/* iterate though the hash. first element is selected with iter_in NULL.
+ * use the returned iterator to access the elements until hash_it_t returns NULL. */
+struct hash_it_t *hash_iterate(struct hashtable_t *hash, struct hash_it_t *iter_in) {
+	struct hash_it_t *iter;
+
+	if (iter_in == NULL) {
+		iter= debugMalloc(sizeof(struct hash_it_t), 301);
+		iter->index =  -1;
+		iter->bucket = NULL;
+		iter->prev_bucket = NULL;
+	} else
+		iter= iter_in;
+
+	/* sanity checks first (if our bucket got deleted in the last iteration): */
+	if (iter->bucket!=NULL) {
+		if (iter->first_bucket != NULL) {
+
+			/* we're on the first element and it got removed after the last iteration. */
+			if ((*iter->first_bucket) != iter->bucket) {
+
+				/* there are still other elements in the list */
+				if ( (*iter->first_bucket) != NULL ) {
+					iter->prev_bucket = NULL;
+					iter->bucket= (*iter->first_bucket);
+					iter->first_bucket = &hash->table[ iter->index ];
+					return(iter);
+				} else {
+					iter->bucket = NULL;
+				}
+
+			}
+
+		} else if ( iter->prev_bucket != NULL ) {
+
+			/* we're not on the first element, and the bucket got removed after the last iteration.
+			* the last bucket's next pointer is not pointing to our actual bucket anymore.
+			* select the next. */
+			if ( iter->prev_bucket->next != iter->bucket )
+				iter->bucket= iter->prev_bucket;
+
+		}
+
+	}
+
+	/* now as we are sane, select the next one if there is some */
+	if (iter->bucket!=NULL) {
+		if (iter->bucket->next!=NULL) {
+			iter->prev_bucket= iter->bucket;
+			iter->bucket= iter->bucket->next;
+			iter->first_bucket = NULL;
+			return(iter);
+		}
+	}
+	/* if not returned yet, we've reached the last one on the index and have to search forward */
+
+	iter->index++;
+	while ( iter->index < hash->size ) {		/* go through the entries of the hash table */
+		if ((hash->table[ iter->index ]) != NULL){
+			iter->prev_bucket = NULL;
+			iter->bucket = hash->table[ iter->index ];
+			iter->first_bucket = &hash->table[ iter->index ];
+			return(iter);						/* if this table entry is not null, return it */
+		} else
+			iter->index++;						/* else, go to the next */
+	}
+	/* nothing to iterate over anymore */
+	debugFree(iter, 1304);
+	return(NULL);
+}
+
+
+/* allocates and clears the hash */
+struct hashtable_t *hash_new(int size, hashdata_compare_cb compare, hashdata_choose_cb choose) {
+	struct hashtable_t *hash;
+
+	hash= debugMalloc( sizeof(struct hashtable_t) , 302);
+	if ( hash == NULL ) 			/* could not allocate the hash control structure */
+		return (NULL);
+
+	hash->size= size;
+	hash->table= debugMalloc( sizeof(struct element_t *) * size, 303);
+	if ( hash->table == NULL ) {	/* could not allocate the table */
+		debugFree(hash, 1305);
+		return(NULL);
+	}
+	hash_init(hash);
+	hash->compare= compare;
+	hash->choose= choose;
+	return(hash);
+}
+
+
+/* adds data to the hashtable. returns 0 on success, -1 on error */
+int hash_add(struct hashtable_t *hash, void *data) {
+	int index;
+	struct element_t *bucket, *prev_bucket = NULL;
+
+	index = hash->choose( data, hash->size );
+	bucket = hash->table[index];
+
+	while (bucket!=NULL) {
+		if (hash->compare(bucket->data, data))
+			return(-1);
+
+		prev_bucket = bucket;
+		bucket= bucket->next;
+	}
+
+	/* found the tail of the list, add new element */
+	if (NULL == (bucket= debugMalloc(sizeof(struct element_t),304)))
+		return(-1); /* debugMalloc failed */
+
+	bucket->data= data;				/* init the new bucket */
+	bucket->next= NULL;
+
+	/* and link it */
+	if ( prev_bucket == NULL ) {
+		hash->table[index] = bucket;
+	} else {
+		prev_bucket->next = bucket;
+	}
+
+	hash->elements++;
+	return(0);
+
+}
+/* finds data, based on the key in keydata. returns the found data on success, or NULL on error */
+void *hash_find(struct hashtable_t *hash, void *keydata) {
+	int index;
+	struct element_t *bucket;
+
+	index = hash->choose( keydata , hash->size );
+	bucket = hash->table[index];
+
+	while ( bucket!=NULL ) {
+		if (hash->compare(bucket->data, keydata))
+			return( bucket->data );
+
+		bucket= bucket->next;
+	}
+
+	return(NULL);
+
+}
+
+/* remove bucket (this might be used in hash_iterate() if you already found the bucket
+ * you want to delete and don't need the overhead to find it again with hash_remove().
+ * But usually, you don't want to use this function, as it fiddles with hash-internals. */
+void *hash_remove_bucket(struct hashtable_t *hash, struct hash_it_t *hash_it_t) {
+	void *data_save;
+
+	data_save = hash_it_t->bucket->data;	/* save the pointer to the data */
+
+	if ( hash_it_t->prev_bucket != NULL ) {
+		hash_it_t->prev_bucket->next = hash_it_t->bucket->next;
+	} else if ( hash_it_t->first_bucket != NULL ) {
+		(*hash_it_t->first_bucket) = hash_it_t->bucket->next;
+	}
+
+	debugFree(hash_it_t->bucket, 1306);
+
+	hash->elements--;
+	return( data_save );
+
+}
+
+
+/* removes data from hash, if found. returns pointer do data on success,
+ * so you can remove the used structure yourself, or NULL on error .
+ * data could be the structure you use with just the key filled,
+ * we just need the key for comparing. */
+void *hash_remove(struct hashtable_t *hash, void *data) {
+	struct hash_it_t hash_it_t;
+
+	hash_it_t.index = hash->choose( data, hash->size );
+	hash_it_t.bucket = hash->table[hash_it_t.index];
+	hash_it_t.prev_bucket = NULL;
+
+	while ( hash_it_t.bucket!=NULL ) {
+		if (hash->compare(hash_it_t.bucket->data, data)) {
+			hash_it_t.first_bucket = (hash_it_t.bucket == hash->table[hash_it_t.index] ? &hash->table[ hash_it_t.index ] : NULL);
+			return( hash_remove_bucket(hash, &hash_it_t) );
+		}
+
+		hash_it_t.prev_bucket = hash_it_t.bucket;
+		hash_it_t.bucket= hash_it_t.bucket->next;
+	}
+
+	return(NULL);
+
+}
+
+
+/* resize the hash, returns the pointer to the new hash or NULL on error. removes the old hash on success. */
+struct hashtable_t *hash_resize(struct hashtable_t *hash, int size) {
+	struct hashtable_t *new_hash;
+	struct element_t *bucket;
+	int i;
+
+	/* initialize a new hash with the new size */
+	if (NULL == (new_hash= hash_new(size, hash->compare, hash->choose)))
+		return(NULL);
+
+	/* copy the elements */
+	for (i=0; i<hash->size; i++) {
+		bucket= hash->table[i];
+		while (bucket != NULL) {
+			hash_add( new_hash, bucket->data );
+			bucket= bucket->next;
+		}
+	}
+	hash_delete(hash, NULL);	/* remove hash and eventual overflow buckets but not the content itself. */
+
+	return( new_hash);
+
+}
+
+
+/* print the hash table for debugging */
+/* void hash_debug(struct hashtable_t *hash) {
+	int i;
+	struct element_t *bucket;
+
+	for (i=0; i<hash->size;i++) {
+		printf("[%d] ",i);
+		bucket= hash->table[i];
+
+		while (bucket != NULL) {
+			printf("-> [%10p] ", (void *)bucket);
+			bucket= bucket->next;
+		}
+
+		printf("\n");
+
+	}
+	printf("\n");
+}*/
+

hash.h
@@ -1,0 +1,95 @@
+/* 
+ * Copyright (C) 2006-2009 B.A.T.M.A.N. contributors:
+ *
+ * Simon Wunderlich, Marek Lindner
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
+ *
+ */
+#ifndef _BATMAN_HASH_H
+#define _BATMAN_HASH_H
+
+
+
+typedef int (*hashdata_compare_cb)(void *, void *);
+typedef int (*hashdata_choose_cb)(void *, int);
+typedef void (*hashdata_free_cb)(void *);
+
+struct element_t {
+	void *data;						/* pointer to the data */
+	struct element_t *next;			/* overflow bucket pointer */
+};
+
+struct hash_it_t {
+	int index;
+	struct element_t *bucket;
+	struct element_t *prev_bucket;
+	struct element_t **first_bucket;
+};
+
+struct hashtable_t {
+	struct element_t **table;					/* the hashtable itself, with the buckets */
+	int elements;								/* number of elements registered */
+	int size;									/* size of hashtable */
+	hashdata_compare_cb compare;			    /* callback to a compare function.
+												 * should compare 2 element datas for their keys,
+												 * return 0 if same and not 0 if not same */
+	hashdata_choose_cb choose;					/* the hashfunction, should return an index based
+												 * on the key in the data of the first argument
+												 * and the size the second */
+};
+
+/* clears the hash */
+void 				 hash_init(struct hashtable_t *hash);
+
+/* allocates and clears the hash */
+struct hashtable_t	*hash_new(int size, hashdata_compare_cb compare, hashdata_choose_cb choose);
+
+/* remove bucket (this might be used in hash_iterate() if you already found the bucket
+ * you want to delete and don't need the overhead to find it again with hash_remove().
+ * But usually, you don't want to use this function, as it fiddles with hash-internals. */
+void 				*hash_remove_bucket(struct hashtable_t *hash, struct hash_it_t *hash_it_t);
+
+/* remove the hash structure. if hashdata_free_cb != NULL,
+ * this function will be called to remove the elements inside of the hash.
+ * if you don't remove the elements, memory might be leaked. */
+void 				 hash_delete(struct hashtable_t *hash, hashdata_free_cb free_cb);
+
+/* free only the hashtable and the hash itself. */
+void 				 hash_destroy(struct hashtable_t *hash);
+
+/* adds data to the hashtable. returns 0 on success, -1 on error */
+int 				 hash_add(struct hashtable_t *hash, void *data);
+
+/* removes data from hash, if found. returns pointer do data on success,
+ * so you can remove the used structure yourself, or NULL on error .
+ * data could be the structure you use with just the key filled,
+ * we just need the key for comparing. */
+void 				*hash_remove(struct hashtable_t *hash, void *data);
+
+/* finds data, based on the key in keydata. returns the found data on success, or NULL on error */
+void 				*hash_find(struct hashtable_t *hash, void *keydata);
+
+/* resize the hash, returns the pointer to the new hash or NULL on error. removes the old hash on success */
+struct hashtable_t	*hash_resize(struct hashtable_t *hash, int size);
+
+/* print the hash table for debugging */
+void 				 hash_debug( struct hashtable_t *hash);
+
+/* iterate though the hash. first element is selected with iter_in NULL.
+ * use the returned iterator to access the elements until hash_it_t returns NULL. */
+struct hash_it_t 	*hash_iterate(struct hashtable_t *hash, struct hash_it_t *iter_in);
+
+#endif

commit a48555e14dacb99eeb31cde8c88d27a5cf67a456

batctl: bat-hosts: free bat_host when hash_add fails
If the hash_add fails (OOM or duplicated entry), then the allocated
bat_host would leak. The user would also not be informed about this
problem.

Check the return value and handle the error to make this problem visible to
the user.

Fixes: c7c76f63c1f9 ("[batctl] integrating batman hash implementation / move bat-hosts stuff into an extra set of files")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

bat-hosts.c
@@ -130,7 +130,14 @@ static void parse_hosts_file(struct hashtable_t **hash, const char path[], int r
 		strncpy(bat_host->name, name, HOST_NAME_MAX_LEN);
 		bat_host->name[HOST_NAME_MAX_LEN - 1] = '\0';
 
-		hash_add(*hash, bat_host);
+		if (hash_add(*hash, bat_host) < 0) {
+			if (read_opt & USE_BAT_HOSTS)
+				fprintf(stderr,
+					"Error - could not add bat host: %s\n",
+					name);
+			free(bat_host);
+			continue;
+		}
 
 		if ((*hash)->elements * 4 > (*hash)->size) {
 			swaphash = hash_resize((*hash), (*hash)->size * 2);
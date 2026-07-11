open-mesh-mirror/batctl

commit 02921f7519a296da09c2679ee0f3cc842b47c6cb

batctl: tcpdump - print subtype for UNICAST4ADDR packets
Signed-off-by: Antonio Quartulli <ordex@autistici.org>
Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>

tcpdump.c
@@ -560,9 +560,10 @@ static void dump_batman_4addr(unsigned char *packet_buff, ssize_t buff_len, int 
 	printf("BAT %s > ",
 	       get_name_by_macaddr((struct ether_addr *)ether_header->ether_shost, read_opt));
 
-	printf("%s: 4ADDR, ttvn %d, ttl %hhu, ",
+	printf("%s: 4ADDR, subtybe %hhu, ttvn %d, ttl %hhu, ",
 	       get_name_by_macaddr((struct ether_addr *)unicast_4addr_packet->u.dest, read_opt),
-	       unicast_4addr_packet->u.ttvn, unicast_4addr_packet->u.header.ttl);
+	       unicast_4addr_packet->subtype, unicast_4addr_packet->u.ttvn,
+	       unicast_4addr_packet->u.header.ttl);
 
 	parse_eth_hdr(packet_buff + ETH_HLEN + sizeof(struct batadv_unicast_4addr_packet),
 		      buff_len - ETH_HLEN - sizeof(struct batadv_unicast_4addr_packet),

commit 8e911500ff96ddec47e53b66cf7613f918a4fbcb

batctl: tcpdump: fix "subtybe" typo in 4ADDR output
The unicast-4addr packet dump prints the field label as "subtybe"
instead of "subtype".

Fixes: 02921f7519a2 ("batctl: tcpdump - print subtype for UNICAST4ADDR packets")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

tcpdump.c
@@ -1184,7 +1184,7 @@ static void dump_batman_4addr(unsigned char *packet_buff, ssize_t buff_len,
 	printf("BAT %s > ",
 	       get_name_by_macaddr((struct ether_addr *)ether_header->ether_shost, read_opt));
 
-	printf("%s: 4ADDR, subtybe %hhu, ttvn %d, ttl %hhu, ",
+	printf("%s: 4ADDR, subtype %hhu, ttvn %d, ttl %hhu, ",
 	       get_name_by_macaddr((struct ether_addr *)unicast_4addr_packet->u.dest, read_opt),
 	       unicast_4addr_packet->subtype, unicast_4addr_packet->u.ttvn,
 	       unicast_4addr_packet->u.ttl);
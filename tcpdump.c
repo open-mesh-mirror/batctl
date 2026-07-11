open-mesh-mirror/batctl

commit fca4ef98aa99c37ed5a925cd1f01daaefbb02612

change mac convert functions, add icmp packet output

tcpdump.c
@@ -19,7 +19,8 @@
 
 void tcpdump_usage() {
 	printf("Battool module tcpdump\n");
-	printf("Usage: battool tcpdump|td interface\n");
+	printf("Usage: battool tcpdump|td [option] interface\n");
+	printf("\t-p packet type\n\t\t1=batman packets\n\t\t2=icmp packets\n\t\t3=unicast packets\n");
 	printf("\t-h help\n");
 	return;
 }
@@ -28,7 +29,17 @@ void print_batman_packet( unsigned char *buf)
 {
 	struct batman_packet *bp = (struct batman_packet *)buf;
 	printf("batman header:\n");
-	printf(" ptype=%d flags=%x ttl=%u orig=%s seq=%u gwflags=%x version=%u\n", bp->packet_type,bp->flags, bp->ttl, ether_ntoa((struct ether_addr*) bp->orig), ntohs(bp->seqno), bp->gwflags, bp->version );
+	printf(" ptype=%u flags=%x ttl=%u orig=%s seq=%u gwflags=%x version=%u\n", bp->packet_type,bp->flags, bp->ttl, ether_ntoa((struct ether_addr*) bp->orig), ntohs(bp->seqno), bp->gwflags, bp->version );
+	return;
+}
+
+void print_icmp_packet( unsigned char *buf)
+{
+	struct icmp_packet *ip = (struct icmp_packet *)buf;
+	printf("icmp header:\n");
+	printf(" ptype=%u mtype=%u ttl=%u dst=%s", ip->packet_type, ip->msg_type, ip->ttl, ether_ntoa((struct ether_addr*) ip->dst ) );
+	printf(" orig=%s seq=%u uid=%u\n", ether_ntoa((struct ether_addr*) ip->orig), ntohs(ip->seqno), ip->uid );
+	return;
 }
 
 void print_packet( int length, unsigned char *buf )
@@ -47,12 +58,15 @@ void print_packet( int length, unsigned char *buf )
 		printf("%02x ", buf[i] );
 	}
 	printf("\n");
+	return;
 }
 
 int tcpdump_main( int argc, char **argv )
 {
 	int  optchar,
-		packetsize = 2000;
+		packetsize = 2000,
+		ptype=0,
+		tmp;
 
 	unsigned char packet[packetsize];
 	uint8_t found_args = 1;
@@ -65,8 +79,14 @@ int tcpdump_main( int argc, char **argv )
 
 	char *devicename;
 
-	while ( ( optchar = getopt ( argc, argv, "h" ) ) != -1 ) {
+	while ( ( optchar = getopt ( argc, argv, "p:h" ) ) != -1 ) {
 		switch( optchar ) {
+			case 'p':
+				tmp = strtol(optarg, NULL , 10);
+				if( tmp > 0 && tmp < 4 )
+					ptype = tmp;
+				found_args+=2;
+				break;
 			case 'h':
 				tcpdump_usage();
 				exit(EXIT_SUCCESS);
@@ -108,17 +128,22 @@ int tcpdump_main( int argc, char **argv )
 
 	while( ( rec_length = read(rawsock,packet,packetsize) ) > 0 ) {
 
-		printf("\n---------------------------------------------------------------------------------------\n\n");
-		printf("ethernet header:\n");
-		printf(" %d bytes dest=%s ", rec_length, ether_ntoa( (struct ether_addr *)eth->ether_dhost ) );
-		printf("src=%s type=%04x\n", ether_ntoa( (struct ether_addr *) eth->ether_shost ),  ntohs( eth->ether_type ) );
+		if( !ptype || packet[sizeof( struct ether_header)] + 1 == ptype ) {
 
-		if( packet[sizeof( struct ether_header)] == 0 )
+			printf("\n---------------------------------------------------------------------------------------\n\n");
+			printf("ethernet header:\n");
+			printf(" %d bytes dest=%s ", rec_length, ether_ntoa( (struct ether_addr *)eth->ether_dhost ) );
+			printf("src=%s type=%04x\n", ether_ntoa( (struct ether_addr *) eth->ether_shost ),  ntohs( eth->ether_type ) );
+
+		}
+
+		if( ( !ptype && packet[sizeof( struct ether_header)] == 0 ) || ( packet[sizeof( struct ether_header)] == 0 && ptype - 1 == 0  ) ) {
 			print_batman_packet( packet + sizeof( struct ether_header ) );
-		else
-			printf("message typ %d currently not supported\n", packet[sizeof( struct ether_header)] );
-
-		print_packet( rec_length, packet );
+			print_packet( rec_length, packet );
+		} else if( ( !ptype && packet[sizeof( struct ether_header)] == 1 ) || ( packet[sizeof( struct ether_header)] == 1 && ptype - 1 == 1 ) ) {
+			print_icmp_packet( packet + sizeof( struct ether_header ) );
+			print_packet( rec_length, packet );
+		}
 
 	}
 

battool.h
@@ -48,3 +48,4 @@ struct hosts {
 
 int ping_main( int argc, char **argv, struct hosts *hosts );
 int traceroute_main( int argc, char **argv, struct hosts *hosts );
+int tcpdump_main( int argc, char **argv );

functions.c
@@ -2,43 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <netinet/ether.h>
 
 #include "functions.h"
 
 int convert_mac( char mac_string[], uint8_t mac[] ) {
-	int i, j=0;
-	char tmp[2];
-
-	if( strlen( mac_string ) > 17 )
+	struct ether_addr *tmp;
+	tmp = ether_aton( mac_string );
+	if( tmp == NULL )
 		return -1;
-
-	for( i = 0; i < strlen( mac_string ) ; ) {
-		if( mac_string[i] != ':' ) {
-			tmp[0] = mac_string[i];
-		} else {
-			mac[j] = 0;
-			i++;
-			j++;
-			continue;
-		}
-
-		if( mac_string[i+1] != ':' ) {
-			tmp[1] = mac_string[i+1];
-			i+=3;
-		} else {
-			tmp[1] = tmp[0];
-			tmp[0] = '0';
-			i+=2;
-		}
-
-		mac[j] = strtol(tmp,NULL,16);
-		j++;
+	else {
+		memcpy( mac, tmp, 6 );
+		return 0;
 	}
-
-	if( j < 5 || j > 6 )
-		return -1;
-
-	return 1;
 }
 
 double time_diff( struct timeval *start, struct timeval *end ) {
@@ -62,5 +38,7 @@ double time_diff( struct timeval *start, struct timeval *end ) {
 }
 
 void convert_mac_i( uint8_t  mac[], char string[] ) {
-	snprintf( string, 17, "%x:%x:%x:%x:%x:%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );
+	char *tmp;
+	tmp = ether_ntoa( (struct ether_addr*)mac );
+	memcpy( string, tmp, 18 );
 }

ping.c
@@ -123,7 +123,7 @@ int ping_main( int argc, char **argv, struct hosts *hosts ) {
 	if( mac_string  == NULL )
 		mac_string = argv[found_args];
 
-	if( convert_mac( mac_string, mac ) < 1 ) {
+	if( convert_mac( mac_string, mac ) < 0 ) {
 		printf("The mac address was not correct.\n");
 		exit(EXIT_FAILURE);
 	}

traceroute.c
@@ -101,7 +101,7 @@ int traceroute_main( int argc, char **argv, struct hosts *hosts ) {
 	if( mac_string  == NULL )
 		mac_string = argv[found_args];
 
-	if( convert_mac( mac_string, mac ) < 1 ) {
+	if( convert_mac( mac_string, mac ) < 0 ) {
 		printf("The mac address was not correct.\n");
 		exit(EXIT_FAILURE);
 	}

commit 9cefbc4c37969c5502494ee68e72381a1694eb8a

batctl: tcpdump: reject invalid packet type arguments
The strtol error handling is rather complicated and it is not only about
checking the return value. The possible error indicators are:

* endptr is NULL
* endptr is not pointing to end delimiter
* endptr is pointing at nptr (because it might have been an empty string)
* returned value is larger/smaller than the expected maximum/minimum value
  range

The first tree conditions were not checked even when it is a potential
problem for multiple places. And the user was never informed about the
parsing errors. The old value was simply used.

Perform the needed validation steps and stop with an user readable error on
parsing problems. At the same time, switch to the unsigned number parsing
function strtoul because no negative numbers are allowed as inputs.

Fixes: fca4ef98aa99 ("change mac convert functions, add icmp packet output")
Fixes: c6ed60c0f6fd ("batctl: tcpdump - add option to select all packet types except specified")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

tcpdump.c
@@ -1506,12 +1506,13 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 	fd_set tmp_wait_sockets;
 	int ret = EXIT_FAILURE;
 	fd_set wait_sockets;
+	unsigned long tmp;
 	struct timeval tv;
 	int max_sock = 0;
 	ssize_t read_len;
+	char *endptr;
 	int optchar;
 	int res;
-	int tmp;
 
 	dump_level = dump_level_all;
 
@@ -1527,14 +1528,28 @@ static int tcpdump(struct state *state __maybe_unused, int argc, char **argv)
 			read_opt &= ~USE_BAT_HOSTS;
 			break;
 		case 'p':
-			tmp = strtol(optarg, NULL, 10);
-			if (tmp > 0 && tmp <= dump_level_all)
-				dump_level = tmp;
+			tmp = strtoul(optarg, &endptr, 10);
+			if (!endptr || *endptr != '\0' || endptr == optarg ||
+			    tmp == 0 || tmp > dump_level_all) {
+				fprintf(stderr,
+					"Error - the supplied packet type is invalid: %s\n",
+					optarg);
+				tcpdump_usage();
+				return EXIT_FAILURE;
+			}
+			dump_level = tmp;
 			break;
 		case 'x':
-			tmp = strtol(optarg, NULL, 10);
-			if (tmp > 0 && tmp <= dump_level_all)
-				dump_level &= ~tmp;
+			tmp = strtoul(optarg, &endptr, 10);
+			if (!endptr || *endptr != '\0' || endptr == optarg ||
+			    tmp == 0 || tmp > dump_level_all) {
+				fprintf(stderr,
+					"Error - the supplied packet type is invalid: %s\n",
+					optarg);
+				tcpdump_usage();
+				return EXIT_FAILURE;
+			}
+			dump_level &= ~tmp;
 			break;
 		default:
 			tcpdump_usage();
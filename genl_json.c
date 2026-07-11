open-mesh-mirror/batctl

commit 882ab0d91468694bd5ff1aa65f6538b00efd8802

batctl: remove support for network coding
The kernel module no longer supports the network coding feature. It was
disabled by default and never considered stable enough for production
use. The userspace tools should therefore stop advertising it as a
viable configuration option.

Signed-off-by: Sven Eckelmann <sven@narfation.org>

genl_json.c
@@ -445,10 +445,6 @@ static const struct nla_policy_json batadv_genl_json[NUM_BATADV_ATTR] = {
 		.name = "multicast_forceflood_enabled",
 		.cb = nljson_print_bool,
 	},
-	[BATADV_ATTR_NETWORK_CODING_ENABLED] = {
-		.name = "network_coding_enabled",
-		.cb = nljson_print_bool,
-	},
 	[BATADV_ATTR_ORIG_INTERVAL] = {
 		.name = "orig_interval",
 		.cb = nljson_print_uint32,

Makefile
@@ -68,7 +68,6 @@ $(eval $(call add_command,multicast_forceflood,y))
 $(eval $(call add_command,multicast_mode,y))
 $(eval $(call add_command,neighbors,y))
 $(eval $(call add_command,neighbors_json,y))
-$(eval $(call add_command,network_coding,y))
 $(eval $(call add_command,orig_interval,y))
 $(eval $(call add_command,originators,y))
 $(eval $(call add_command,originators_json,y))

event.c
@@ -282,10 +282,6 @@ static void event_parse_set_mesh(struct nlattr **attrs)
 		printf("* multicast_forceflood %s\n",
 		       u8_to_boolstr(attrs[BATADV_ATTR_MULTICAST_FORCEFLOOD_ENABLED]));
 
-	if (attrs[BATADV_ATTR_NETWORK_CODING_ENABLED])
-		printf("* network_coding %s\n",
-		       u8_to_boolstr(attrs[BATADV_ATTR_NETWORK_CODING_ENABLED]));
-
 	if (attrs[BATADV_ATTR_ORIG_INTERVAL])
 		printf("* orig_interval %u ms\n",
 		       nla_get_u32(attrs[BATADV_ATTR_ORIG_INTERVAL]));

loglevel.c
@@ -36,7 +36,6 @@ static void log_level_usage(void)
 	fprintf(stderr, " \t tt      Messages related to translation table operations\n");
 	fprintf(stderr, " \t bla     Messages related to bridge loop avoidance\n");
 	fprintf(stderr, " \t dat     Messages related to arp snooping and distributed arp table\n");
-	fprintf(stderr, " \t nc      Messages related to network coding\n");
 	fprintf(stderr, " \t mcast   Messages related to multicast\n");
 	fprintf(stderr, " \t tp      Messages related to throughput meter\n");
 }
@@ -145,8 +144,6 @@ static int loglevel(struct state *state, int argc, char **argv)
 				log_level_globals.log_level |= BIT(3);
 			} else if (strcmp(argv[i], "dat") == 0) {
 				log_level_globals.log_level |= BIT(4);
-			} else if (strcmp(argv[i], "nc") == 0) {
-				log_level_globals.log_level |= BIT(5);
 			} else if (strcmp(argv[i], "mcast") == 0) {
 				log_level_globals.log_level |= BIT(6);
 			} else if (strcmp(argv[i], "tp") == 0) {
@@ -177,8 +174,6 @@ static int loglevel(struct state *state, int argc, char **argv)
 	       "messages related to bridge loop avoidance", "bla");
 	printf("[%c] %s (%s)\n", (log_level_globals.log_level & BIT(4)) ? 'x' : ' ',
 	       "messages related to arp snooping and distributed arp table", "dat");
-	printf("[%c] %s (%s)\n", (log_level_globals.log_level & BIT(5)) ? 'x' : ' ',
-	       "messages related to network coding", "nc");
 	printf("[%c] %s (%s)\n", (log_level_globals.log_level & BIT(6)) ? 'x' : ' ',
 	       "messages related to multicast", "mcast");
 	printf("[%c] %s (%s)\n", (log_level_globals.log_level & BIT(7)) ? 'x' : ' ',

man/batctl.8
@@ -325,7 +325,7 @@ If no parameter is given the current log level settings are displayed otherwise 
 level. Level 'none' disables all verbose logging. Level 'batman' enables messages related to routing / flooding / broadcasting.
 Level 'routes' enables messages related to routes being added / changed / deleted. Level 'tt' enables messages related to
 translation table operations. Level 'bla' enables messages related to the bridge loop avoidance. Level 'dat' enables
-messages related to ARP snooping and the Distributed Arp Table. Level 'nc' enables messages related to network coding.
+messages related to ARP snooping and the Distributed Arp Table.
 Level 'mcast' enables messages related to multicast optimizations. Level 'tp' enables messages related to throughput meter.
 Level 'all' enables all messages. The messages are sent to the kernels trace buffers. Use \fBtrace-cmd stream -e batadv:batadv_dbg\fP
 to receive the system wide log messages.
@@ -340,10 +340,6 @@ If no parameter is given the current multicast forceflood setting is displayed. 
 disable multicast forceflood. This setting defines whether multicast optimizations should be replaced by simple broadcast-like
 flooding of multicast packets. If set to non-zero then all nodes in the mesh are going to use classic flooding for any
 multicast packet with no optimizations.
-.TP
-[\fBmeshif\fP \fInetdev\fP] \fBnetwork_coding\fP|\fBnc\fP [\fI0\fP|\fI1\fP]
-If no parameter is given the current network coding mode setting is displayed. Otherwise the parameter is used to enable or
-disable network coding.
 .TP
 [\fBmeshif\fP \fInetdev\fP] \fBorig_interval\fP|\fBit\fP [\fIinterval\fP]
 If no parameter is given the current originator interval setting is displayed otherwise the parameter is used to set the

netlink.c
@@ -215,9 +215,6 @@ struct nla_policy batadv_netlink_policy[NUM_BATADV_ATTR] = {
 	[BATADV_ATTR_MULTICAST_FORCEFLOOD_ENABLED] = {
 		.type = NLA_U8,
 	},
-	[BATADV_ATTR_NETWORK_CODING_ENABLED] = {
-		.type = NLA_U8,
-	},
 	[BATADV_ATTR_ORIG_INTERVAL] = {
 		.type = NLA_U32,
 	},

network_coding.c
@@ -1,60 +1,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/* Copyright (C) B.A.T.M.A.N. contributors:
- *
- * Martin Hundebøll <martin@hundeboll.net>
- *
- * License-Filename: LICENSES/preferred/GPL-2.0
- */
-
-#include "main.h"
-
-#include <errno.h>
-#include <linux/genetlink.h>
-#include <netlink/genl/genl.h>
-
-#include "batman_adv.h"
-#include "netlink.h"
-#include "sys.h"
-
-static struct simple_boolean_data network_coding;
-
-static int print_network_coding(struct nl_msg *msg, void *arg)
-{
-	return sys_simple_print_boolean(msg, arg,
-					BATADV_ATTR_NETWORK_CODING_ENABLED);
-}
-
-static int get_network_coding(struct state *state)
-{
-	return sys_simple_nlquery(state, BATADV_CMD_GET_MESH,
-				  NULL, print_network_coding);
-}
-
-static int set_attrs_network_coding(struct nl_msg *msg, void *arg)
-{
-	struct state *state = arg;
-	struct settings_data *settings = state->cmd->arg;
-	struct simple_boolean_data *data = settings->data;
-
-	nla_put_u8(msg, BATADV_ATTR_NETWORK_CODING_ENABLED, data->val);
-
-	return 0;
-}
-
-static int set_network_coding(struct state *state)
-{
-	return sys_simple_nlquery(state, BATADV_CMD_SET_MESH,
-				  set_attrs_network_coding, NULL);
-}
-
-static struct settings_data batctl_settings_network_coding = {
-	.data = &network_coding,
-	.parse = parse_simple_boolean,
-	.netlink_get = get_network_coding,
-	.netlink_set = set_network_coding,
-};
-
-COMMAND_NAMED(SUBCOMMAND_MIF, network_coding, "nc", handle_sys_setting,
-	      COMMAND_FLAG_MESH_IFACE | COMMAND_FLAG_NETLINK,
-	      &batctl_settings_network_coding,
-	      "[0|1]             \tdisplay or modify network_coding setting");

commit 96b0c7dc42e8cf4a4fecaa57ad0aa94598aa3ceb

batctl: genl_json: drop leftover network coding log level
Support for the network coding feature was removed from batctl but missed
in the genl_json loglevel output.

Fixes: 882ab0d91468 ("batctl: remove support for network coding")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

genl_json.c
@@ -219,8 +219,6 @@ static void nljson_print_loglevel(struct nlattr *attrs[], int idx)
 	       val & BIT(3) ? "true" : "false");
 	printf("\"dat\": %s,",
 	       val & BIT(4) ? "true" : "false");
-	printf("\"nc\": %s,",
-	       val & BIT(5) ? "true" : "false");
 	printf("\"mcast\": %s,",
 	       val & BIT(6) ? "true" : "false");
 	printf("\"tp\": %s,",
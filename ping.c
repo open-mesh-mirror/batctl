open-mesh-mirror/batctl

commit 2ecb2c8b060b63f801d9070633806a0ba5e6ff3f

batctl: tr / ping - ignore packets with wrong sequence number
Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>

ping.c
@@ -196,6 +196,7 @@ int ping(char *mesh_iface, int argc, char **argv)
 			goto sleep;
 		}
 
+read_packet:
 		start_timer();
 
 		FD_ZERO(&read_socket);
@@ -228,6 +229,10 @@ int ping(char *mesh_iface, int argc, char **argv)
 				packet_len, read_len);
 			goto sleep;
 		}
+
+		/* after receiving an unexpected seqno we keep waiting for our answer */
+		if (htons(seq_counter) != icmp_packet_in.seqno)
+			goto read_packet;
 
 		switch (icmp_packet_in.msg_type) {
 		case ECHO_REPLY:

traceroute.c
@@ -142,6 +142,7 @@ int traceroute(char *mesh_iface, int argc, char **argv)
 				continue;
 			}
 
+read_packet:
 			start_timer();
 
 			tv.tv_sec = 2;
@@ -167,6 +168,10 @@ int traceroute(char *mesh_iface, int argc, char **argv)
 					sizeof(icmp_packet_in), read_len);
 				continue;
 			}
+
+			/* after receiving an unexpected seqno we keep waiting for our answer */
+			if (htons(seq_counter) != icmp_packet_in.seqno)
+				goto read_packet;
 
 			switch (icmp_packet_in.msg_type) {
 			case ECHO_REPLY:

commit 8983dc388306906afb1b951b11fb9b6395feed8e

batctl: ping/traceroute: don't restart RTT timer on stray replies
When a reply with an unexpected seqno arrives (for example a late answer to
an already timed out request), ping and traceroute jump back to read_packet
to keep waiting for the real answer. The jump target also re-runs
start_timer(), so the reported round trip time only measures from the
arrival of the stray packet instead of from the transmission of the
request, underreporting the RTT and polluting the min/avg/max statistics.

Start the timer (and initialize the timeout in traceroute) once per
transmitted request. At the same time, let select in icmp_interface_read()
the wait time instead of restarting it each time.

Fixes: 2ecb2c8b060b ("batctl: tr / ping - ignore packets with wrong sequence number")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

ping.c
@@ -209,9 +209,9 @@ static int ping(struct state *state, int argc, char **argv)
 
 		packets_out++;
 
-read_packet:
 		start_timer();
 
+read_packet:
 		read_len = icmp_interface_read((struct batadv_icmp_header *)&icmp_packet_in,
 					       packet_len, &tv);
 

traceroute.c
@@ -137,12 +137,12 @@ static int traceroute(struct state *state, int argc, char **argv)
 				continue;
 			}
 
-read_packet:
 			start_timer();
 
 			tv.tv_sec = 2;
 			tv.tv_usec = 0;
 
+read_packet:
 			read_len = icmp_interface_read((struct batadv_icmp_header *)&icmp_packet_in,
 						       sizeof(icmp_packet_in), &tv);
 			if (read_len <= 0)